ba0ccc136e606cdb5f164a4bf855f74f085b22a9
mkan <m@kos.co.th>
2022-10-12 18:27:28 +0700

    feat : setting base project

--- src/pages/Home.test.tsx
+++ src/pages/Home.test.tsx
@@ -1,5 +1,8 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+
 import '@testing-library/jest-dom';
+
 import Home from './index';
 
 it('Should render title text', () => {
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
-import type { AppProps } from 'next/app';
 import React from 'react';
+import type { AppProps } from 'next/app';
+
 import '../styles/globals.css';
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
+const MyApp = ({ Component, pageProps }: AppProps)  => {
   return <Component {...pageProps} />;
 };
 

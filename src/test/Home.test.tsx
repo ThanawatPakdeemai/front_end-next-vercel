a2e721f3c94ab36d00939f1d62d78e8ec1f97f2d
deWhiteMaple <github@cryptopunt.com>
2022-11-07 15:59:53 +0700

    Update to next v.13

--- src/test/Home.test.tsx
+++ src/test/Home.test.tsx
@@ -1,13 +1,11 @@
 import React from "react"
 import { render, screen } from "@testing-library/react"
-
 import "@testing-library/jest-dom"
-
 import Home from "../pages/index"
 
 describe("Check Home Component", () => {
   it("Should render title text", () => {
     render(<Home />)
-    expect(screen.getByText("Next.js!")).toBeInTheDocument()
+    expect(screen.getByLabelText("Next.js!")).toBeInTheDocument()
   })
 })

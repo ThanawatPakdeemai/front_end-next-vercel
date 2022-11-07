09b70a4ac69f0b2fc13037a148ece848b5f1c54c
deWhiteMaple <github@cryptopunt.com>
2022-11-07 15:59:53 +0700

    Update to next v.13

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 const nextJest = require("next/jest")
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: "./"
 })
 
@@ -25,5 +24,4 @@ const customJestConfig = {
   // }
 }
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 module.exports = createJestConfig(customJestConfig)

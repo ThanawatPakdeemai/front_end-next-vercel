65c968951cd24ef6504d6ba3da881640ca097c2c
deWhiteMaple <github@cryptopunt.com>
2022-11-14 18:29:47 +0700

    add axios config

--- src/features/authentication/container/service/profile.services.ts
+++ src/features/authentication/container/service/profile.services.ts
@@ -1,13 +1,8 @@
-// import {
-//   IAuthService,
-//   IProfileSignUp
-// } from "@feature/profile/interfaces/IProfileServices"
-// import Config from "@src/configs"
-// import services from "@src/configs/axiosGlobalConfig"
-// import useProfileStore from "@src/stores/profileStore"
-// import helper from "@src/utils/helper"
-// import axios from "axios"
-// import { IRefreshToken } from "../../interface/IAuth"
+import axios from "axios"
+import Config from "@src/configs"
+import { IRefreshToken } from "@src/interfaces/IAuth"
+import useProfileStore from "@src/stores/profileStore"
+import helper from "@src/utils/helper"
 
 export const signIn = (_address: string, _signature: string) =>
   new Promise<any>((resolve, reject) => {
@@ -31,26 +26,28 @@ export const signOut = async () =>
 
 export const refreshProfileToken = async (
   callBeckWhenError?: () => void
-): Promise<any | undefined> => {}
-// try {
-//   const response = await axios.post<{ data: string }>(
-//     `${Config.NEXT_PUBLIC_API_URL}/auth/refresh-token`,
-//     {},
-//     {
-//       withCredentials: true
-//     }
-//   )
-//   const result = helper.decryptWithAES(response.data.data)
-//   return JSON.parse(result)
-// } catch (error) {
-//   useProfileStore.getState().onReset()
-//   helper.removeTokenInLocal()
-//   callBeckWhenError && callBeckWhenError()
-//   if (error instanceof Error) {
-//     console.error("Error", error.message)
-//     throw Error(`refreshToken : ${error}`)
-//   }
-// }
-//   return {}
-// }
-// end of refreshProfileToken --
+): Promise<any | undefined> => {
+  try {
+    const response = await axios.post<IRefreshToken>(
+      `${Config.NEXT_PUBLIC_API_URL}/auth/refresh-token`,
+      {},
+      {
+        withCredentials: true
+      }
+    )
+    localStorage.setItem("token", response.data.jwtToken)
+    axios.defaults.headers.common = {
+      Authorization: `Bearer ${response.data.jwtToken}`
+    }
+
+    return response.data.jwtToken
+  } catch (error) {
+    useProfileStore.getState().onReset()
+    helper.resetLocalStorage()
+    callBeckWhenError && callBeckWhenError()
+    if (error instanceof Error) {
+      console.error("Error", error.message)
+      throw Error(`refreshToken : ${error}`)
+    }
+  }
+}

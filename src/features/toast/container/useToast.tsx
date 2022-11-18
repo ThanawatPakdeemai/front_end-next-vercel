6a9f89f275d012c3b2b84ffc75dd49773b735678
deWhiteMaple <github@cryptopunt.com>
2022-11-18 16:07:18 +0700

    add toast provider

---
+++ src/features/toast/container/useToast.tsx
@@ -0,0 +1,101 @@
+import IconInfo from "@src/components/icons/InfoIcon"
+import CloseCircledIcon from "@src/components/icons/CloseCircledIcon"
+import React, { useCallback } from "react"
+import toast from "react-hot-toast"
+import { BaseToastComponent } from "../components"
+
+const useToast = () => {
+  const successToast = useCallback((content: string) => {
+    toast(
+      (t) => (
+        <BaseToastComponent
+          type="success"
+          onClose={() => toast.dismiss(t.id)}
+          typeIcon={<IconInfo.Ori className="fill-neutral-07" />}
+          closeButtonIcon={<CloseCircledIcon.Ori className="fill-neutral-07" />}
+          content={content}
+        />
+      ),
+      {
+        id: `success-${new Date()}`
+      }
+    )
+  }, [])
+  const infoToast = useCallback((content: string) => {
+    toast(
+      (t) => (
+        <BaseToastComponent
+          type="info"
+          onClose={() => toast.dismiss(t.id)}
+          typeIcon={<IconInfo.Ori className="fill-neutral-01" />}
+          closeButtonIcon={<CloseCircledIcon.Ori className="fill-neutral-01" />}
+          content={content}
+        />
+      ),
+      {
+        id: `success-${new Date()}`
+      }
+    )
+  }, [])
+
+  const warnToast = useCallback((content: string) => {
+    toast(
+      (t) => (
+        <BaseToastComponent
+          type="warning"
+          onClose={() => toast.dismiss(t.id)}
+          typeIcon={<IconInfo.Ori className="fill-neutral-07" />}
+          closeButtonIcon={<CloseCircledIcon.Ori className="fill-neutral-07" />}
+          content={content}
+        />
+      ),
+      {
+        id: `success-${new Date()}`
+      }
+    )
+  }, [])
+
+  const errorToast = useCallback((content: string) => {
+    toast(
+      (t) => (
+        <BaseToastComponent
+          type="error"
+          onClose={() => toast.dismiss(t.id)}
+          typeIcon={<IconInfo.Ori className="fill-neutral-01" />}
+          closeButtonIcon={<CloseCircledIcon.Ori className="fill-neutral-01" />}
+          content={content}
+        />
+      ),
+      {
+        id: `success-${new Date()}`
+      }
+    )
+  }, [])
+
+  const sampleToast = useCallback((content: string) => {
+    toast(
+      (t) => (
+        <BaseToastComponent
+          type="default"
+          onClose={() => toast.dismiss(t.id)}
+          typeIcon={<IconInfo.Ori className="fill-neutral-07" />}
+          closeButtonIcon={<CloseCircledIcon.Ori className="fill-neutral-07" />}
+          content={content}
+        />
+      ),
+      {
+        id: `success-${new Date()}`
+      }
+    )
+  }, [])
+
+  return {
+    successToast,
+    infoToast,
+    warnToast,
+    errorToast,
+    sampleToast
+  }
+}
+
+export default useToast

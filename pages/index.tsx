eba64e10d95816703a36af535d2f2cf34f07ad3c
deWhiteMaple <github@cryptopunt.com>
2022-11-21 16:24:30 +0700

    update example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,21 @@
 import { Layout } from "@src/components/template"
 import { ReactElement } from "react"
+import Image from "@components/atoms/image"
+// import Image from "@components/atoms/image"
 
 export default function Home() {
+  const imgSrc = "/images/mocks/footer.png"
+
   return (
     <article className="h-full w-full">
-      <></>
+      <>
+        <Image
+          src={imgSrc}
+          alt=""
+          width={1023}
+          height={452}
+        />
+      </>
     </article>
   )
 }

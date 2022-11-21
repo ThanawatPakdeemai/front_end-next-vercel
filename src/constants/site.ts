6380dde569b272b4553426ee75d037255c53bbf5
deWhiteMaple <github@cryptopunt.com>
2022-11-21 14:51:37 +0700

    add image constant, remove unusable file

--- src/constants/site.ts
+++ src/constants/site.ts
@@ -1,9 +1,9 @@
-export const baseUrl = {
-  mode: process.env.NEXT_MODE,
-  api: process.env.NEXT_API_URL,
-  game: process.env.NEXT_GAME_URL,
-  baseSite: process.env.NEXT_FRONTEND_URL,
-  support: process.env.NEXT_SUPPORT,
-  marketplace: process.env.NEXT_MARKETPLACE,
-  nakaverse: process.env.NEXT_NAKAVERSE
+export const BASE_URL = {
+  mode: process.env.NEXT_PUBLIC_MODE,
+  api: process.env.NEXT_PUBLIC_API_URL,
+  game: process.env.NEXT_PUBLIC_GAME_URL,
+  baseSite: process.env.NEXT_PUBLIC_FRONTEND_URL,
+  support: process.env.NEXT_PUBLIC_SUPPORT,
+  marketplace: process.env.NEXT_PUBLIC_MARKETPLACE,
+  nakaverse: process.env.NEXT_PUBLIC_NAKAVERSE
 }

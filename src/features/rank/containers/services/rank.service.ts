a4a50ae2f72e1cedfada371ef7e379b57a590d57
Buchit Baltip <tiger@kos.co.th>
2022-11-22 13:41:17 +0700

    fix : type

--- src/features/rank/containers/services/rank.service.ts
+++ src/features/rank/containers/services/rank.service.ts
@@ -1,11 +1,11 @@
 import services from "@src/configs/axiosGlobalConfig"
 import { BASE_URL } from "@src/constants/site"
-import { IRank } from "../../Interfaces/IRank"
+import { IProfile } from "@src/features/profile/interfaces/IProfileServices"
 
 const email = localStorage.getItem("email")
 
 const updateRank = (game_id: string) =>
-  new Promise<IRank>((resolve, reject) => {
+  new Promise<IProfile>((resolve, reject) => {
     if (game_id) {
       services
         .get(`${BASE_URL.api}/profile/rank/${game_id}/${email}`)

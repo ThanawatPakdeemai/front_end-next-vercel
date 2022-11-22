cbc70ae03dd77eb17859a874f196e7ffbfcb5a7d
deWhiteMaple <github@cryptopunt.com>
2022-11-22 11:11:50 +0700

    get image data from constant

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,17 @@
 import { Layout } from "@src/components/template"
 import { ReactElement } from "react"
 import Image from "@components/atoms/image"
-// import Image from "@components/atoms/image"
+import { IMAGES } from "@constants/images"
 
 export default function Home() {
-  const imgSrc = "/images/mocks/footer.png"
-
   return (
     <article className="h-full w-full">
       <>
         <Image
-          src={imgSrc}
-          alt=""
-          width={1023}
-          height={452}
+          src={IMAGES.footerMock.src}
+          width={IMAGES.footerMock.width}
+          height={IMAGES.footerMock.height}
+          alt={IMAGES.footerMock.alt}
         />
       </>
     </article>

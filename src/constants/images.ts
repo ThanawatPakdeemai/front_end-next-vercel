43fa0c470750f92b39dac57ba6a1c7224f63f713
deWhiteMaple <github@cryptopunt.com>
2022-11-22 11:11:50 +0700

    get image data from constant

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,1 +1,9 @@
-export const IMAGES = {}
+export const IMAGES = {
+  footerMock: {
+    src: "images/mocks/footer.png",
+    blurDataURL: "images/mocks/footer.png",
+    width: 1023,
+    height: 452,
+    alt: ""
+  }
+}

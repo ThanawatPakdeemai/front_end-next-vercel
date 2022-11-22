139b4e3ee9805bb0a60f85977e32966cacdc8279
Buchit Baltip <tiger@kos.co.th>
2022-11-22 17:19:34 +0700

    fix : localStorage and type.

--- src/features/rank/containers/services/rank.service.ts
+++ src/features/rank/containers/services/rank.service.ts
@@ -1,8 +1,9 @@
 import services from "@src/configs/axiosGlobalConfig"
 import { BASE_URL } from "@src/constants/site"
-import { IProfile } from "@src/features/profile/interfaces/IProfileServices"
+import { IProfile } from "@src/features/profile/interfaces/IProfileService"
+import helper from "@src/utils/helper"
 
-const email = localStorage.getItem("email")
+const email = helper.getLocalStorage("email")
 
 const updateRank = (game_id: string) =>
   new Promise<IProfile>((resolve, reject) => {

416a6fe8b5ab0364cba41be52524d7be6e258877
aofject <aof@kos.co.th>
2022-11-23 16:40:34 +0700

    feat: add quest api

---
+++ src/features/quest/containers/services/quest.service.ts
@@ -0,0 +1,22 @@
+import services from "@src/configs/axiosGlobalConfig"
+import { IFormatMessageService } from "@src/interfaces/IHelper"
+import { IQuestService } from "../../interfaces/IQuest"
+
+export const getAllQuest = (_playerId: string) =>
+  new Promise<IQuestService>((resolve, reject) => {
+    services
+      .get<IQuestService>(`/profile/quest/${_playerId}`)
+      .then((reponse) => resolve(reponse.data))
+      .catch((error) => reject(error))
+  })
+
+export const claimQuestById = (_questId: string) =>
+  new Promise<IFormatMessageService>((resolve, reject) => {
+    const data = { quest_id: _questId }
+    services
+      .post<IFormatMessageService>(`/profile/quest/claim-quest-rewards`, {
+        ...data
+      })
+      .then((reponse) => resolve(reponse.data))
+      .catch((error) => reject(error))
+  })

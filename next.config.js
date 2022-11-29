429ac6ca468075f0bfca6da7878b88aab1d80980
buaykosdesign <buay@kos.co.th>
2022-11-29 16:23:39 +0700

    update next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /**
  * @type {import('next').NextConfig}
  */
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true"
+})
+
 const { i18n } = require("./next-i18next.config")
 
 const nextConfig = {
@@ -15,9 +20,5 @@ const nextConfig = {
   },
   i18n
 }
-
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true"
-})
 
 module.exports = withBundleAnalyzer({ ...nextConfig })

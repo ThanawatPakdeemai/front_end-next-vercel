979edd8ad19b6da485d1195ba01f4960b3eb1659
rrrd1998 <buey@kos.co.th>
2022-12-01 17:15:53 +0700

    feat : zustand game

--- src/stores/game/index.ts
+++ src/stores/game/index.ts
@@ -1,1 +1,8 @@
 import create from "zustand"
+import { IGame } from "@src/features/game/interfaces/IGameService"
+
+export interface IUseGameItemStore {
+  data: IGame[]
+  setGameFree: (_data: IGame[]) => void
+  clearGameFree: () => void
+}

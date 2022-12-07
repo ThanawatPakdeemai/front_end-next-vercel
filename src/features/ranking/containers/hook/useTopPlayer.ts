d54cd6b840e46a210e734c71bd0d2dca5b1f82a4
yui <jaibun.s89@gmail.com>
2022-12-07 12:25:19 +0700

    fix: useQuery

--- src/features/ranking/containers/hook/useTopPlayer.ts
+++ src/features/ranking/containers/hook/useTopPlayer.ts
@@ -3,14 +3,10 @@ import { useQuery } from "@tanstack/react-query"
 import { getPlayersRanking } from "../services/ranking.service"
 
 export const useTopPlayer = () => {
-  const { data, status, isLoading, isSuccess } = useQuery<IPlayerRanking[]>({
+  const { data: topPlayerAllGame } = useQuery<IPlayerRanking[]>({
     queryKey: ["topPlayer"],
     queryFn: () => getPlayersRanking("game/ranks-all").then((res) => res)
   })
-  let topPlayerAllGame: IPlayerRanking[] = []
-  if (isSuccess && status && !isLoading) {
-    topPlayerAllGame = data
-  }
 
   return { topPlayerAllGame }
 }

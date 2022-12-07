8f01bc40b8eb9b236cf03168d3bd84923b17e09d
yui <jaibun.s89@gmail.com>
2022-12-07 17:07:33 +0700

    fix: remove file to atomic design

--- src/features/home/components/template/developerPart.tsx
+++ src/features/home/components/template/developerPart.tsx
@@ -1,6 +1,10 @@
+import { ButtonLink } from "@components/atoms/buttonLink"
+import CardTitle from "@components/organisms/cardTitle"
 import TopPlayer from "@feature/ranking/components/template/topPlayer"
-import { Grid } from "@mui/material"
+import { Card, Grid, Typography } from "@mui/material"
 import { memo } from "react"
+import AddIcon from "@mui/icons-material/Add"
+import DvrIcon from "@mui/icons-material/Dvr"
 import Developer from "../organisms/developer"
 
 const developerPart = () => (
@@ -14,7 +18,28 @@ const developerPart = () => (
         xs={12}
         md={8}
       >
-        <Developer />
+        <Card className="bg-black-02 p-2 sm:h-auto md:h-[465px]">
+          <CardTitle
+            width="100%"
+            icon={<DvrIcon className="mr-2 text-black-01" />}
+            title={
+              <Typography
+                variant="inherit"
+                className="text-black-01"
+              >{`Looking for NAKA<DEVELOPERs>`}</Typography>
+            }
+            background="red"
+            rightTitle={
+              <ButtonLink
+                href="/"
+                text="Register"
+                icon={<AddIcon />}
+                color="error"
+              />
+            }
+          />
+          <Developer />
+        </Card>
       </Grid>
       <Grid
         item

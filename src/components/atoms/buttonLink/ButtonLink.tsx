0a554a4620d8eb4431ec89a860c936a8e24fdd63
mkan <m@kos.co.th>
2022-12-07 17:54:09 +0700

    feat: reverted commit

--- src/components/atoms/buttonLink/ButtonLink.tsx
+++ src/components/atoms/buttonLink/ButtonLink.tsx
@@ -1,39 +1,36 @@
 import { Button } from "@mui/material"
 import Link from "next/link"
 import React from "react"
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward"
 
 export interface IButtonLink extends React.HTMLAttributes<HTMLDivElement> {
   text?: string
   href: string
   icon?: React.ReactNode
-  color?: "primary" | "secondary" | "inherit" | "success" | "error"
+  variant?: "text" | "outlined" | "contained"
+  color?: "primary" | "secondary" | "success" | "error" | "info" | "warning"
+  size?: "small" | "medium" | "large"
+  className?: string
 }
 
 const ButtonLink = ({
-  text = "Text",
+  text,
   href,
   icon,
-  color = "primary"
+  variant,
+  color,
+  size,
+  className
 }: IButtonLink) => (
   <Link
     href={href}
     data-testid="button-link"
   >
     <Button
-      className="button-global"
-      variant="contained"
+      variant={variant}
       color={color}
-      sx={{
-        minWidth: "auto",
-        width: "100%"
-      }}
-      startIcon={<div className="button-icon">{icon}</div>}
-      endIcon={
-        <div className="button-arrow hidden">
-          <ArrowForwardIcon />
-        </div>
-      }
+      size={size}
+      startIcon={icon}
+      className={`${className}`}
     >
       {text}
     </Button>

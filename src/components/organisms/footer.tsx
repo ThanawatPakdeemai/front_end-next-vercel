6c7c9adf2197aa49a2bc5765aa8221c7ba17016b
rrrd1998 <buey@kos.co.th>
2022-12-13 14:21:42 +0700

    feat : footer

--- src/components/organisms/footer.tsx
+++ src/components/organisms/footer.tsx
@@ -1,17 +1,19 @@
 import { Card, Divider, Typography } from "@mui/material"
-import { ButtonLink } from "@src/components/atoms/buttonLink"
 import DvrOutlinedIcon from "@mui/icons-material/DvrOutlined"
 import LocalAtmOutlinedIcon from "@mui/icons-material/LocalAtmOutlined"
 import NorthOutlinedIcon from "@mui/icons-material/NorthOutlined"
 import WineBarOutlinedIcon from "@mui/icons-material/WineBarOutlined"
 import ArrowOutwardOutlinedIcon from "@mui/icons-material/ArrowOutwardOutlined"
-import ButtonLinkOutLine from "@components/atoms/buttonLink/ButtonLinkOutLine"
+import ButtonLink from "@components/atoms/button/ButtonLink"
+import Image from "next/image"
+import { SOCIAL } from "@constants/socialShare"
+import Link from "next/link"
 
 const Footer = () => (
   <>
     <Divider sx={{ marginTop: 10, marginBottom: 10 }} />
-    <div className="flex justify-between text-[12px]">
-      <div className="flex">
+    <div className="justify-between text-[12px] md:flex">
+      <div className="flex justify-center">
         <div className="w-48">
           <div className="mb-4 uppercase text-white-primary">game</div>
           <Typography className="text-[10px] uppercase text-black-default">
@@ -58,64 +60,49 @@ const Footer = () => (
           </div>
         </div>
       </div>
-
-      <div className="w-[500px] justify-self-end">
-        <div className="mb-4 uppercase text-white-primary">
-          BE A PART OF THE PLAY TO EARN REVOLUTION!
-        </div>
-        Join the
-        {/* industry's  */}
-        first comprehensive Play to Earn ecosystem and explore the many benefits
-        it has to offer.
-        <div className="my-8 w-[280px]">
-          <ButtonLink
+      <div className="flex justify-center pt-[20px] md:justify-start md:p-0">
+        <div className="flex flex-col items-center justify-self-end md:w-2/4 md:items-start">
+          <div className="mb-4 uppercase text-white-primary">
+            BE A PART OF THE PLAY TO EARN REVOLUTION!
+          </div>
+          Join the industry&apos;s first comprehensive Play to Earn ecosystem
+          and explore the many benefits it has to offer.
+          <div className="my-8 w-[280px]">
+            <ButtonLink
+              href="/"
+              text="join The Revolutions"
+              icon={<WineBarOutlinedIcon />}
+              size="large"
+              color="secondary"
+              variant="contained"
+              // className="w-full"
+            />
+            {/* <ButtonLink
             href="/"
             text="Join The Revolutions"
             icon={<WineBarOutlinedIcon />}
             color="secondary"
-          />
-        </div>
-        <div className="flex">
-          <Card
-            sx={{
-              color: "#E1E2E2",
-              padding: 1.5,
-              borderRadius: 0.5,
-              marginRight: 1,
-              backgroundColor: "#18181C",
-              border: 1,
-              borderColor: "#232329"
-            }}
-          >
-            <DvrOutlinedIcon />
-          </Card>
-          <Card
-            sx={{
-              padding: 1.5,
-              borderRadius: 0.5,
-              marginRight: 1
-            }}
-          >
-            <DvrOutlinedIcon />
-          </Card>
-          <Card sx={{ padding: 1.5, borderRadius: 0.5, marginRight: 1 }}>
-            <DvrOutlinedIcon />
-          </Card>
-          <Card sx={{ padding: 1.5, borderRadius: 0.5, marginRight: 1 }}>
-            <DvrOutlinedIcon />
-          </Card>
-          <Card sx={{ padding: 1.5, borderRadius: 0.5, marginRight: 1 }}>
-            <DvrOutlinedIcon />
-          </Card>
-          <Card sx={{ padding: 1.5, borderRadius: 0.5, marginRight: 1 }}>
-            <DvrOutlinedIcon />
-          </Card>
-          <Card sx={{ padding: 1.5, borderRadius: 0.5, marginRight: 1 }}>
-            <DvrOutlinedIcon />
-          </Card>
-          <Card sx={{ padding: 1.5, borderRadius: 0.5, marginRight: 1 }}>
-            <DvrOutlinedIcon />
-          </Card>
+          /> */}
+          </div>
+          {/* grid grid-cols-7 */}
+          <div className="flex">
+            {SOCIAL?.map((item) => (
+              <Link
+                key={item.label}
+                href={item.href}
+                target="_blank"
+              >
+                <div className="m-1 flex h-[50px] w-[50px] items-center justify-center rounded-lg bg-[#18181C] ">
+                  <Image
+                    src={item.img}
+                    alt={item.label}
+                    width={item.width}
+                    height={item.height}
+                  />
+                </div>
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
     </div>
@@ -127,29 +114,33 @@ const Footer = () => (
         }}
       >
         <div
-          className="flex"
+          className="md:flex"
           text-white-primary
         >
           <div className="w-[400px]">
-            <ButtonLinkOutLine
+            <ButtonLink
               href="/"
               text="Become a Naka Devs"
               icon={<DvrOutlinedIcon />}
-              color="primary"
+              size="large"
+              color="secondary"
+              variant="outlined"
+              // className="w-full"
             />
           </div>
           <h3 className="pl-[30px] text-grey-neutral04">
-            Join the
-            {/* industry's */}
-            first comprehensive Play to Earn ecosystem.
+            Join the industry&apos;s first comprehensive Play to Earn ecosystem.
           </h3>
 
           <div className="w-[400px]">
-            <ButtonLinkOutLine
+            <ButtonLink
               href="/"
               text="Become a Partner"
               icon={<LocalAtmOutlinedIcon />}
-              color="primary"
+              size="large"
+              color="secondary"
+              variant="outlined"
+              // className="w-full"
             />
           </div>
           <h3 className="pl-[30px] text-grey-neutral04">

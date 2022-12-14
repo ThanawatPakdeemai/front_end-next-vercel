784f260967b9f7424e14b9e6a2dcb1879625aee4
aofject <aof@kos.co.th>
2022-12-15 04:16:12 +0700

    feat: show game carousel

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,15 @@ import { Image } from "@components/atoms/image/index"
 import { IMAGES } from "@constants/images"
 import TopPlayer from "@feature/ranking/components/template/topPlayer"
 import { BannerSlide } from "@feature/home/components/organisms/bannerSlide"
+import {
+  mockF2PGame,
+  F2PHeaderMenu,
+  mockP2EGame,
+  P2EHeaderMenu
+} from "@constants/gameSlide"
+import IconFree from "@components/icons/freeIcon"
+import IconNakaWorld from "@components/icons/nakaWorldIcon"
+import GameCarousel from "@components/molecules/gameSlide/GameCarousel"
 
 export default function Home() {
   return (
@@ -19,6 +28,36 @@ export default function Home() {
           alt={IMAGES.footerMock.alt}
         />
         <TopPlayer />
+        <div className="my-20 h-full w-full">
+          <GameCarousel
+            list={mockF2PGame}
+            tag={F2PHeaderMenu.title}
+            headerMenu={F2PHeaderMenu.menuList}
+            theme={F2PHeaderMenu.theme}
+            checkTimer
+            headerIcon={
+              <IconFree
+                width={200}
+                height={100}
+              />
+            }
+          />
+        </div>
+        <div className="my-20 h-full w-full">
+          <GameCarousel
+            list={mockP2EGame}
+            showNo
+            tag={P2EHeaderMenu.title}
+            headerMenu={P2EHeaderMenu.menuList}
+            theme={P2EHeaderMenu.theme}
+            headerIcon={
+              <IconNakaWorld
+                width={180}
+                height={125}
+              />
+            }
+          />
+        </div>
       </>
     </article>
   )
---
+++ src/components/molecules/gameSlide/GameCarousel.tsx
@@ -0,0 +1,229 @@
+import React, { memo, useRef, useState } from "react"
+import Slider, { Settings } from "react-slick"
+import ImageCustom from "@components/atoms/image/Image"
+import { Chip } from "@mui/material"
+import SportsEsportsOutlinedIcon from "@mui/icons-material/SportsEsportsOutlined"
+import GameCarouselHeader, {
+  ISlideList
+} from "@components/molecules/gameSlide/GameCarouselHeader"
+import TimerStamina from "@components/atoms/timer/TimerStamina"
+import { motion } from "framer-motion"
+import IconHourglass from "@components/icons/hourglassIcon"
+import ButtonToggleIcon from "./ButtonToggleIcon"
+
+export interface ISlide {
+  id: number
+  image: string
+  desc: string
+}
+
+interface IProps {
+  list: ISlide[]
+  tag: string
+  showNo?: boolean
+  headerIcon: React.ReactNode
+  headerMenu: ISlideList[]
+  theme: string
+  checkTimer?: boolean
+}
+
+type TColor =
+  | "default"
+  | "primary"
+  | "secondary"
+  | "error"
+  | "info"
+  | "success"
+  | "warning"
+
+const GameCarousel = ({
+  list,
+  showNo = false,
+  tag,
+  headerIcon,
+  headerMenu,
+  theme,
+  checkTimer = false
+}: IProps) => {
+  const staminaRecovery = new Date("2022-12-14T22:24:00.000Z")
+
+  const [cooldown, setCooldown] = useState<boolean>(false)
+
+  const orderColor = (_no: number) => {
+    if (_no === 1) {
+      return "bg-red-card"
+    }
+    if (_no === 2) {
+      return "bg-polygon-default"
+    }
+    if (_no === 3) {
+      return "bg-green-card"
+    }
+    return "bg-grey-A100 border-2 border-grey-900"
+  }
+
+  const showSlide = list && list.length > 6 ? 6 : list.length
+
+  const settings: Settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: showSlide,
+    slidesToScroll: showSlide,
+    arrows: false,
+    variableWidth: false
+  }
+
+  const sliderRef = useRef<Slider>(null)
+
+  const onSlideNext = () => {
+    sliderRef?.current?.slickNext()
+  }
+  const onSlidePrev = () => {
+    sliderRef?.current?.slickPrev()
+  }
+
+  const onViewAll = () => {}
+
+  const onHandleClick = () => {}
+
+  const cardImg = {
+    init: {
+      scale: 1
+    },
+    onHover: {
+      scale: [1, 0.94, 0.96, 0.94],
+      transition: {
+        duration: 0.4
+      }
+    }
+  }
+
+  const btnCard = {
+    init: {
+      y: 40,
+      opacity: 0
+    },
+    onHover: {
+      y: -8,
+      opacity: 1,
+      transition: {
+        duration: 0.1,
+        type: "spring",
+        stiffness: 600
+      }
+    }
+  }
+
+  const onChipColor = (_theme: string | undefined) => {
+    let chip: TColor = "default"
+    const chipThemeList: Array<TColor> = [
+      "default",
+      "primary",
+      "secondary",
+      "error",
+      "info",
+      "success",
+      "warning"
+    ]
+    const chipThemeMapping = chipThemeList.find((v) => v === _theme)
+    if (chipThemeMapping) {
+      chip = chipThemeMapping
+    }
+    return chip
+  }
+
+  return (
+    <div className="mb-10">
+      <GameCarouselHeader
+        icon={headerIcon}
+        title={tag}
+        menuList={headerMenu}
+        onView={onViewAll}
+        onNext={onSlideNext}
+        onPrev={onSlidePrev}
+        theme={theme}
+      />
+      <Slider
+        ref={sliderRef}
+        {...settings}
+      >
+        {list &&
+          list.map((item, index) => (
+            <motion.div
+              key={`${item.id}_game`}
+              className="slick-card-container"
+              initial="init"
+              whileHover="onHover"
+              animate="animate"
+            >
+              <motion.div
+                className="relative flex h-[218px] w-full overflow-hidden"
+                variants={cardImg}
+              >
+                {showNo ? (
+                  <div
+                    className={`${orderColor(
+                      index + 1
+                    )} absolute top-0 right-0 m-[10px] flex h-10 w-10 items-center justify-center rounded-sm font-neue-machina text-default font-bold text-white-primary`}
+                  >
+                    {index + 1}
+                  </div>
+                ) : null}
+                <ImageCustom
+                  src={item.image}
+                  alt="home-slide"
+                  width={218}
+                  height={218}
+                  className="rounded-md"
+                />
+                <motion.div
+                  variants={btnCard}
+                  className="absolute bottom-0 flex w-full justify-center text-white-primary"
+                >
+                  <ButtonToggleIcon
+                    startIcon={
+                      cooldown ? (
+                        <IconHourglass />
+                      ) : (
+                        <SportsEsportsOutlinedIcon />
+                      )
+                    }
+                    text={cooldown ? "cooldown..." : "play now"}
+                    handleClick={onHandleClick}
+                    className={`btn-rainbow-theme z-[2] w-[198px] ${
+                      cooldown ? "bg-error-main" : "bg-secondary-main "
+                    } capitalize`}
+                  />
+                </motion.div>
+              </motion.div>
+              <div className="relative z-[3]">
+                <div className="slick-card-desc flex h-10 w-full items-center">
+                  <p className="relative truncate uppercase hover:text-clip">
+                    {item.desc}
+                  </p>
+                </div>
+                <div className="relative grid w-full grid-cols-2 gap-2 text-xs uppercase">
+                  <Chip
+                    label={tag}
+                    size="small"
+                    color={onChipColor(theme)}
+                    className="font-bold"
+                  />
+                  {checkTimer ? (
+                    <TimerStamina
+                      time={staminaRecovery}
+                      show={cooldown}
+                      setShow={setCooldown}
+                    />
+                  ) : null}
+                </div>
+              </div>
+            </motion.div>
+          ))}
+      </Slider>
+    </div>
+  )
+}
+
+export default memo(GameCarousel)

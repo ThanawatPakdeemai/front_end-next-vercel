858dbda7b8dff1658c72ea7d99f960a85154a935
Hatto <tnwycr@gmail.com>
2022-12-15 15:40:00 +0700

    feat : add tagline in homePage

--- src/features/page/homePage.tsx
+++ src/features/page/homePage.tsx
@@ -1,6 +1,7 @@
 import IconFree from "@components/icons/freeIcon"
 import IconNakaWorld from "@components/icons/nakaWorldIcon"
 import GameCarousel from "@components/molecules/gameSlide/GameCarousel"
+import Tagline from "@components/molecules/tagline/Tagline"
 import {
   F2PHeaderMenu,
   mockF2PGame,
@@ -16,6 +17,15 @@ const Home = () => (
   <>
     <BannerSlide />
     {/* Testing display a CarouselSlide component, waiting to merge with team */}
+    <Tagline
+      bgColor="bg-secondary-main"
+      textColor="text-white-default"
+      text="Secue. fun. simple. earn $naka AND enjoy "
+      alt="logo_master"
+      src="/assets/icons/logo_master.png"
+      width={24}
+      height={24}
+    />
     <div className="grid grid-cols-2 gap-6">
       <></>
       <CarouselSlide />
@@ -51,6 +61,15 @@ const Home = () => (
         }
       />
     </div>
+    <Tagline
+      bgColor="bg-info-main"
+      textColor="text-neutral-800 font-bold"
+      text="Show your God Mode for the blockchain gaming landscape"
+      alt="logo_shape"
+      src="/assets/icons/logo_shape.png"
+      width={24}
+      height={24}
+    />
   </>
 )
 export default memo(Home)

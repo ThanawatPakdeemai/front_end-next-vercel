755897e4c3a1b6f060c58e9f72884afd85b21305
Buchit Baltip <tiger@kos.co.th>
2022-12-20 15:17:17 +0700

    fix: image and animation

--- src/constants/categories.ts
+++ src/constants/categories.ts
@@ -1,31 +1,31 @@
 export const CATEGORIES = [
   {
     link: "/",
-    img: "/images/categories/actionGame.png",
+    img: "/images/categories/actionGame.webp",
     text: "Action Games",
     icon: "/images/categories/axe.svg"
   },
   {
     link: "/",
-    img: "/images/categories/shootingGame.png",
+    img: "/images/categories/shootingGame.webp",
     text: "Shooting Games",
     icon: "/images/categories/collapse.svg"
   },
   {
     link: "/",
-    img: "/images/categories/sportGame.png",
+    img: "/images/categories/sportGame.webp",
     text: "Sport Games",
     icon: "/images/categories/basketball.svg"
   },
   {
     link: "/",
-    img: "/images/categories/nftGame.png",
+    img: "/images/categories/nftGame.webp",
     text: "NFT Games",
     icon: "/images/categories/diamond.svg"
   },
   {
     link: "/",
-    img: "/images/categories/otherGame.png",
+    img: "/images/categories/otherGame.webp",
     text: "Other Games",
     icon: "/images/categories/grid.svg"
   }

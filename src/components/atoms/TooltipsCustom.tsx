32d269835b8a64c453844bd38342aaff429da806
Hatto <tnwycr@gmail.com>
2022-12-22 10:55:49 +0700

    fix : override style mui

--- src/components/atoms/TooltipsCustom.tsx
+++ src/components/atoms/TooltipsCustom.tsx
@@ -5,12 +5,19 @@ interface IProp extends TooltipProps {
   color: "primary" | "secondary" | "error" | "warning" | "info" | "success"
 }
 
+/**
+ * @description Default placement is "bottom"
+ * @param title Should always send
+ */
+
 const TooltipsCustom = ({ color, ...props }: IProp) => (
   <Tooltip
     arrow
     classes={{
       tooltip: `bg-${color}-main text-${color}-contrastText uppercase font-neue-machina-bold py-[10px] px-4 rounded-lg`,
-      arrow: `text-${color}-main`
+      arrow: `text-${color}-main`,
+      tooltipPlacementRight: "!mr-0 ml-[14px]",
+      tooltipPlacementLeft: "mr-[14px] !ml-0"
     }}
     {...props}
   >

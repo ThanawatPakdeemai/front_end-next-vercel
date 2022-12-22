d208269bd6e507b366c97a3d271463c1999e5549
aofject <aof@kos.co.th>
2022-12-22 12:08:46 +0700

    feat: add ButtonCircle

---
+++ src/components/atoms/button/ButtonCircle.tsx
@@ -0,0 +1,70 @@
+import { IconButton } from "@mui/material"
+import React, { ReactNode } from "react"
+
+interface IProps {
+  icon: ReactNode
+  dotSize?: "small" | "medium"
+  dotClassName?: string
+  multiCircle?: boolean
+  notify?: boolean
+  onClick?: () => void
+}
+
+const ButtonCircle = ({
+  icon,
+  dotSize = "small",
+  dotClassName,
+  multiCircle = false,
+  notify = false,
+  onClick
+}: IProps) => {
+  const onHandleClick = async () => {
+    if (onClick) {
+      await onClick()
+    }
+  }
+  if (multiCircle) {
+    return (
+      <IconButton
+        className="sticky-btn-container"
+        disableRipple
+        onClick={onHandleClick}
+      >
+        <div className="sticky-btn-secondary">
+          <div className="sticky-btn relative">
+            {notify ? (
+              <div
+                className={`${dotClassName} absolute bg-error-main ${
+                  dotSize === "small"
+                    ? "top-[8%] right-[8%] h-[6px] w-[6px]"
+                    : "top-[4%] right-[4%] h-[10px] w-[10px]"
+                } rounded-sm `}
+              />
+            ) : null}
+            {icon}
+          </div>
+        </div>
+      </IconButton>
+    )
+  }
+  return (
+    <IconButton
+      className="sticky-btn"
+      disableRipple
+      onClick={onHandleClick}
+    >
+      {notify ? (
+        <div
+          className={`${dotClassName} absolute bg-error-main ${
+            dotSize === "small"
+              ? "top-[8%] right-[8%] h-[6px] w-[6px]"
+              : "top-[4%] right-[4%] h-[10px] w-[10px]"
+          } rounded-sm `}
+        />
+      ) : null}
+      {icon}
+    </IconButton>
+  )
+}
+
+export default ButtonCircle

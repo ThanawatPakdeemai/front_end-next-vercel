2556bdde9719d518f2eea009729313d833ad3401
F.Boy <>
2022-12-26 18:22:38 +0700

    feat : page login

---
+++ pages/login.tsx
@@ -0,0 +1,115 @@
+import React from "react"
+import { Grid } from "@mui/material"
+// import LoginForm from "../../components/LoginForm"
+
+const backgroundURL = "/img/others/img-17.jpg"
+const backgroundStyle = {
+  backgroundImage: `url(${backgroundURL})`,
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover"
+}
+
+const Login = (props) => (
+  // <div className="h-100 bg-white">
+  //   <Grid
+  //     container
+  //     justifyContent="center"
+  //     alignItems="center"
+  //     className="align-items-stretch h-100"
+  //   >
+  //     <Grid
+  //      item
+  //       xs={10}
+  //       sm={10}
+  //       md={12}
+  //       lg={8}
+  //     >
+  //       <div className="d-flex flex-column justify-content-center h-100 container">
+  //         <Grid
+  //            container
+  //           justifyContent="center">
+  //           <Grid
+  //           item
+  //             xs={12}
+  //             sm={12}
+  //             md={10}
+  //             lg={6}
+  //             xl={4}
+  //           >
+  //             <h1>Sign In</h1>
+  //             <p>
+  //               Don't have an account yet?{" "}
+  //               <a href="/auth/register-2">Sign Up</a>
+  //             </p>
+  //             <div className="mt-4">{/* <LoginForm {...props} /> */}</div>
+  //           </Grid>
+  //         </Grid>
+  //       </div>
+  //     </Grid>
+  //     <Grid
+  //     item
+  //       xs={0}
+  //       sm={0}
+  //       md={0}
+  //       lg={4}
+  //     >
+  //       <div
+  //         className="d-flex flex-column justify-content-between h-100 px-4"
+  //         style={backgroundStyle}
+  //       >
+  //         <div className="text-right">
+  //           <img
+  //             src="/img/logo-white.png"
+  //             alt="logo"
+  //           />
+  //         </div>
+  //         <Grid
+  //         container
+  //     justifyContent="center">
+  //           <Grid
+  //           item
+  //             xs={0}
+  //             sm={0}
+  //             md={0}
+  //             lg={10}
+  //           >
+  //             <img
+  //               className="img-fluid mb-5"
+  //               src="/img/others/img-18.png"
+  //               alt=""
+  //             />
+  //             <h1 className="text-white">Welcome to emilus</h1>
+  //             <p className="text-white">
+  //               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+  //               ullamcorper nisl erat, vel convallis elit fermentum
+  //               pellentesque.
+  //             </p>
+  //           </Grid>
+  //         </Grid>
+  //         <div className="d-flex justify-content-end pb-4">
+  //           <div>
+  //             <a
+  //               className="text-white"
+  //               href="/#"
+  //               onClick={(e) => e.preventDefault()}
+  //             >
+  //               Term & Conditions
+  //             </a>
+  //             <span className="text-white mx-2"> | </span>
+  //             <a
+  //               className="text-white"
+  //               href="/#"
+  //               onClick={(e) => e.preventDefault()}
+  //             >
+  //               Privacy & Policy
+  //             </a>
+  //           </div>
+  //         </div>
+  //       </div>
+  //     </Grid>
+  //   </Row>
+  // </div>
+  <>dsdsa</>
+)
+
+export default Login

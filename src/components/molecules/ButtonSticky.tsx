e334a480c442b578f97f83980496a19eed4a90c6
aofject <aof@kos.co.th>
2022-12-26 11:50:35 +0700

    feat: add support & mission icon on HomePage

--- src/components/molecules/ButtonSticky.tsx
+++ src/components/molecules/ButtonSticky.tsx
@@ -3,36 +3,41 @@ import React, { ReactNode } from "react"
 
 interface IProps {
   icon?: ReactNode
+  notify?: boolean
   multi?: boolean
   className?: string
 }
 
 const ButtonSticky = ({
   icon = <YourMissionIcon />,
+  notify = false,
   multi = false,
   className
-}: IProps) => (
-  <button
-    type="button"
-    className={`${className} relative z-[5] flex h-[88px] w-[88px] items-center justify-center`}
-  >
-    {multi ? (
-      <>
-        <div className="btn-sticky-out flex items-center justify-center" />
-        <div className="btn-sticky-in flex items-center justify-center" />
-        <div className="btn-sticky-icon flex items-center justify-center">
-          {icon}
-        </div>
-        <div className="btn-sticky-dot" />
-      </>
-    ) : (
-      <>
-        <div className="btn-sticky-icon flex items-center justify-center">
-          {icon}
-        </div>
-        <div className="btn-sticky-dot" />
-      </>
-    )}
-  </button>
-)
+}: IProps) => {
+  const hasNotify = notify ? <div className="btn-sticky-dot" /> : null
+  return (
+    <button
+      type="button"
+      className={`${className} relative z-[5] flex h-[88px] w-[88px] items-center justify-center`}
+    >
+      {multi ? (
+        <>
+          <div className="btn-sticky-out flex items-center justify-center" />
+          <div className="btn-sticky-in flex items-center justify-center" />
+          <div className="btn-sticky-icon flex items-center justify-center">
+            {icon}
+          </div>
+          {hasNotify}
+        </>
+      ) : (
+        <>
+          <div className="btn-sticky-icon flex items-center justify-center">
+            {icon}
+          </div>
+          {hasNotify}
+        </>
+      )}
+    </button>
+  )
+}
 export default ButtonSticky

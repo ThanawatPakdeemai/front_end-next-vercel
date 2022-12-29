65876e8ad50cf1a0fd5109465d4c555ac7150885
yui <jaibun.s89@gmail.com>
2022-12-29 10:40:12 +0700

    fix: file

--- utils/configDevtools.ts
+++
@@ -1,9 +0,0 @@
-function configZustandDevTools(name: string, actionName?: string) {
-  return {
-    name: `${name}`,
-    enabled: process.env.NODE_ENV === "development",
-    anonymousActionType: `${actionName || name}`
-  }
-}
-
-export default configZustandDevTools

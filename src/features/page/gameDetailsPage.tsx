89ddcde4196f0fade66d11d0efd33613952f86b8
Hatto <tnwycr@gmail.com>
2023-01-03 16:39:57 +0700

    feat : add tagline in gamepagedetail

--- src/features/page/gameDetailsPage.tsx
+++ src/features/page/gameDetailsPage.tsx
@@ -1,4 +1,6 @@
+import ShineIcon from "@components/icons/ShineIcon"
 import LikeNoLobby from "@components/molecules/LikeNoLobby"
+import Tagline from "@components/molecules/tagline/Tagline"
 import React, { useState } from "react"
 
 /**
@@ -10,6 +12,12 @@ const GameDetailsPage = () => {
 
   return (
     <>
+      <Tagline
+        bgColor="bg-neutral-800"
+        textColor="text-neutral-500 font-bold"
+        text="Don't miss the information analysis about this game"
+        icon={<ShineIcon />}
+      />
       <LikeNoLobby value={percentageOfLike} />
     </>
   )

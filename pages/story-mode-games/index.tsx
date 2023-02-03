f326f1b10c26b2629cd547a35f6bc035cf2059c6
buaykosdesign <buay@kos.co.th>
2023-02-03 14:24:53 +0700

    update filename

--- pages/story-mode-games/index.tsx
+++ pages/story-mode-games/index.tsx
@@ -1,13 +1,13 @@
 import { ReactElement } from "react"
 import { serverSideTranslations } from "next-i18next/serverSideTranslations"
 import GamePageLayout from "@components/template/GamePageLayout"
-import StoryModeGamesPage from "@feature/page/games/StoryModeGamesPage"
+// import StoryModeGamesPage from "@feature/page/games/StoryModeGamesPage"
 
 export default function StoryModeGames() {
   return (
     <>
       <article className="h-full w-full">
-        <StoryModeGamesPage />
+        {/* <StoryModeGamesPage /> */}
       </article>
     </>
   )

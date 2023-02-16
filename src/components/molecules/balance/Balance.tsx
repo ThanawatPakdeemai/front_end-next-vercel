7c1001ace0975612c4e0ee6b6d5a84ad6f90df6f
yui <yui@kos.co.th>
2023-02-16 17:20:37 +0700

    fix: css balance

--- src/components/molecules/balance/Balance.tsx
+++ src/components/molecules/balance/Balance.tsx
@@ -1,9 +1,7 @@
 import { useEffect, useState } from "react"
-import SyncAltIcon from "@mui/icons-material/SyncAlt"
 import { Card, CardContent, SxProps, Theme } from "@mui/material"
 import INaka from "@components/icons/Naka"
 import IBusd from "@components/icons/Busd"
-import ButtonIcon from "@components/atoms/button/ButtonIcon"
 import useProfileStore from "@stores/profileStore"
 import Metamask from "@components/atoms/metamask"
 import ButtonLink from "@components/atoms/button/ButtonLink"
@@ -13,6 +11,7 @@ import { useWeb3Provider } from "@providers/index"
 import useGetBalanceVault from "@feature/inventory/containers/hooks/useGetBalanceVault"
 import { useRouter } from "next/router"
 import Helper from "@utils/helper"
+import AmountBalance from "./AmountBalance"
 
 interface IProps {
   token?: string | undefined
@@ -22,24 +21,10 @@ interface IProps {
   sx?: SxProps<Theme> | undefined
 }
 
-const iconmotion = {
-  hover: {
-    scale: 1.2,
-    rotate: 17,
-    ease: "easeIn",
-    transition: {
-      duration: 0.4,
-      stiffness: 500,
-      type: "spring"
-    }
-  }
-}
-
 const Balance = ({ className, sx }: IProps) => {
   const profile = useProfileStore((state) => state.profile.data)
   const [nakaBalanceVault, SetNakaBalanceVault] = useState<string>("N/A")
   const { address, handleConnectWithMetamask, hasMetamask } = useWeb3Provider()
-  const router = useRouter()
   const { t } = useTranslation()
   const { balanceVaultNaka } = useGetBalanceVault(
     profile?.address ?? "",
@@ -129,57 +114,17 @@ const Balance = ({ className, sx }: IProps) => {
             className={`my-2 min-w-[200px] items-center justify-center p-0 ${className}`}
           >
             <Card
-              className=" m-auto gap-[5px] rounded-[13px] bg-neutral-800  px-[5px] py-[5px] "
+              className=" m-auto flex-row gap-y-3  rounded-[13px] bg-neutral-800  px-[5px] pt-[5px] "
               sx={sx}
             >
-              <div className="mb-1 flex items-center justify-between">
-                <div className="flex h-full flex-1 items-center rounded-lg bg-neutral-900 py-[11px] px-[10px]">
-                  <INaka />
-                  <p className="ml-6 text-sm font-bold text-white-primary">
-                    {nakaBalanceVault}
-                  </p>
-                  {/* <BalanceVault
-                  variant={variant}
-                  className="ml-6 text-sm font-bold text-white-primary"
-                /> */}
-                </div>
-                <ButtonIcon
-                  variants={iconmotion}
-                  whileHover="hover"
-                  transition={{ type: "spring", stiffness: 400, damping: 4 }}
-                  icon={
-                    <SyncAltIcon className="h-[20px] w-[20px] rotate-90 text-white-primary" />
-                  }
-                  className="ml-1 flex h-[40px] w-[40px] items-center justify-center rounded-lg border border-neutral-700 bg-neutral-900"
-                  onClick={() => {
-                    router.push("/wallet")
-                  }}
-                />
-              </div>
-              <div className=" flex items-center justify-between">
-                <div className="flex h-full flex-1 items-center rounded-lg bg-neutral-900 py-[5px] px-[10px]">
-                  <IBusd width={21} />
-                  <p className="ml-6 text-sm font-bold text-white-primary">
-                    {Busd}
-                  </p>
-                  {/* <BalanceVault
-                  variant={variant}
-                  className="ml-6 text-sm font-bold text-white-primary"
-                /> */}
-                </div>
-                <ButtonIcon
-                  variants={iconmotion}
-                  whileHover="hover"
-                  transition={{ type: "spring", stiffness: 400, damping: 4 }}
-                  icon={
-                    <SyncAltIcon className="h-[20px] w-[20px] rotate-90 text-white-primary" />
-                  }
-                  className="ml-1 flex h-[40px] w-[40px] items-center justify-center rounded-lg border border-neutral-700 bg-neutral-900"
-                  onClick={() => {
-                    router.push("/wallet")
-                  }}
-                />
-              </div>
+              <AmountBalance
+                balance={nakaBalanceVault}
+                icon={<INaka />}
+              />
+              <AmountBalance
+                balance={Busd}
+                icon={<IBusd width={21} />}
+              />
             </Card>
           </CardContent>
         </>

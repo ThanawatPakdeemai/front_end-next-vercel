de6ee2e07c7b249fa1f0d0fdf9beb870abcb9507
mkan <m@kos.co.th>
2023-03-01 17:05:08 +0700

    style: improved reponsive Arcade game detail page

--- src/components/templates/game/BuyItemBody.tsx
+++ src/components/templates/game/BuyItemBody.tsx
@@ -2,7 +2,7 @@ import { Box } from "@mui/material"
 import React from "react"
 
 const BuyItemBody = ({ children }: { children: React.ReactNode }) => (
-  <Box className="mt-2 flex flex-[1_1_340px] gap-2 md:mt-0 md:flex-none lg:w-[333px]">
+  <Box className="mt-2 flex h-full flex-[1_1_340px] flex-wrap gap-2 md:mt-0 lg:w-[333px] lg:flex-none">
     {children}
   </Box>
 )

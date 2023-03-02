ff674000ae5a4c733e38e66ffaab5e85f64bb7e5
yui <yui@kos.co.th>
2023-03-02 10:47:51 +0700

    fix: link link summary game

--- src/features/game/components/templates/roomList/singlePlayer/SingleRoomList.tsx
+++ src/features/game/components/templates/roomList/singlePlayer/SingleRoomList.tsx
@@ -69,16 +69,18 @@ const GameRoomList = () => {
         _dataRoom.amount_current_player < _dataRoom.max_players
       ) {
         if (data && data_player_me && data_player_me.status === "played") {
-          router.push(`/${data.path}/summary/${_roomId}`)
+          router.push(
+            `/${router?.query?.typeGame}/${data.path}/summary/${_roomId}`
+          )
         } else {
-          router.push(`${router.asPath}/${_roomId}`)
+          router.push(`/${router?.query?.typeGame}/${router.asPath}/${_roomId}`)
         }
       } else if (new Date() > new Date(_dataRoom.end_time)) {
         errorToast(MESSAGES["room-timeout"])
       } else if (_dataRoom.amount_current_player >= _dataRoom.max_players) {
         errorToast(MESSAGES["room-full"])
       } else if (data && (data.play_to_earn || data.tournament)) {
-        router.push(`${router.asPath}/${_roomId}`)
+        router.push(`/${router?.query?.typeGame}/${router.asPath}/${_roomId}`)
       } else if (
         (balanceofItem && balanceofItem?.data < 1) ||
         balanceofItem === undefined

9d1777fd5eaa12ac0ada4e827e27d64d2b7df3cd
Hatto <tnwycr@gmail.com>
2023-03-17 18:26:35 +0700

    feat : apply skeleton load

--- src/features/page/marketplace/MarketplaceCardList.tsx
+++ src/features/page/marketplace/MarketplaceCardList.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect } from "react"
+import React from "react"
 import { v4 as uuidv4 } from "uuid"
 import dynamic from "next/dynamic"
 import { useNakaPriceProvider } from "@providers/NakaPriceProvider"
 import { PaginationNaka } from "@components/atoms/pagination"
-import useLoadingStore from "@stores/loading"
 import useMarketplace from "@hooks/useMarketplace"
+import SkeletonItem from "@feature/marketplace/components/molecules/SkeletonItem"
 
 const CardItemMarketPlace = dynamic(
   () => import("@components/molecules/cards/CardItemMarketPlace"),
@@ -14,7 +14,6 @@ const CardItemMarketPlace = dynamic(
 )
 
 const MarketplaceCardList = () => {
-  const { setOpen, setClose } = useLoadingStore()
   const { price } = useNakaPriceProvider()
   const {
     orderData,
@@ -26,56 +25,64 @@ const MarketplaceCardList = () => {
     setCurrentPage
   } = useMarketplace()
 
-  // mockup for skeleton
-  useEffect(() => {
-    if (isLoading) {
-      setOpen()
-    } else {
-      setClose()
-    }
-  }, [isLoading, setClose, setOpen])
-
-  return (
-    <div className="flex flex-col gap-y-7">
-      <div className="grid w-full grid-cols-1 gap-x-3 gap-y-7 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-        {orderData &&
-          orderData.data.map((_data, _index) => (
-            <CardItemMarketPlace
-              key={uuidv4()}
-              cardType={type}
-              id={_data.land_data?.land_id}
-              idLink={_data._id}
-              itemAmount={_data.building_data ? _data.item_amount : undefined}
-              itemTotal={_data.building_data ? _data.item_total : undefined}
-              itemImage={
-                _data.building_data && {
-                  src: _data.building_data.image,
-                  alt: _data.building_data.name,
-                  width: 250,
-                  height: 250
+  if (orderData && orderData.data.length > 0 && !isLoading) {
+    return (
+      <div className="flex flex-col gap-y-7">
+        <div className="grid w-full grid-cols-1 gap-x-3 gap-y-7 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+          {orderData &&
+            orderData.data.length !== 0 &&
+            !isLoading &&
+            orderData.data.map((_data, _index) => (
+              <CardItemMarketPlace
+                key={uuidv4()}
+                cardType={type}
+                id={_data.land_data?.land_id}
+                idLink={_data._id}
+                itemAmount={_data.building_data ? _data.item_amount : undefined}
+                itemTotal={_data.building_data ? _data.item_total : undefined}
+                itemImage={
+                  _data.building_data && {
+                    src: _data.building_data.image,
+                    alt: _data.building_data.name,
+                    width: 250,
+                    height: 250
+                  }
                 }
-              }
-              itemVideo={
-                _data.land_data && {
-                  src: _data.land_data.NFT_video,
-                  poster: _data.land_data.NFT_image
+                itemVideo={
+                  _data.land_data && {
+                    src: _data.land_data.NFT_video,
+                    poster: _data.land_data.NFT_image
+                  }
                 }
-              }
-              itemName={_data.land_data?.name || _data.building_data?.name}
-              itemLevel={_data.building_data?.level}
-              price={
-                (_data.price / (price ? parseFloat(price.last) : 0)) as number
-              }
-            />
-          ))}
+                itemName={_data.land_data?.name || _data.building_data?.name}
+                itemLevel={_data.building_data?.level}
+                price={
+                  (_data.price / (price ? parseFloat(price.last) : 0)) as number
+                }
+              />
+            ))}
+        </div>
+        <PaginationNaka
+          totalCount={totalCount}
+          limit={limit}
+          page={currentPage}
+          setPage={setCurrentPage}
+        />
       </div>
-      <PaginationNaka
-        totalCount={totalCount}
-        limit={limit}
-        page={currentPage}
-        setPage={setCurrentPage}
-      />
-    </div>
+    )
+  }
+  return (
+    <>
+      {orderData?.data.length === 0 && !isLoading ? (
+        <div>No data</div>
+      ) : (
+        <div className="grid w-full grid-cols-1 gap-x-3 gap-y-7 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+          {[...Array(limit)].map(() => (
+            <SkeletonItem key={uuidv4()} />
+          ))}
+        </div>
+      )}
+    </>
   )
 }
 

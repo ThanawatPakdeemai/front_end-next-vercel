7117591e2365d362d6c417d7929aaf96fb36c285
rrrd1998 <buey@kos.co.th>
2023-03-20 17:10:28 +0700

    feat: add motion

--- src/features/page/inventory/InventoryPage.tsx
+++ src/features/page/inventory/InventoryPage.tsx
@@ -1,6 +1,7 @@
 import SettingIconFilter from "@components/icons/Inventory/SettingIconFilter"
 import { Box } from "@mui/material"
 import React, { useState } from "react"
+import { motion } from "framer-motion"
 import HighlightOffRoundedIcon from "@mui/icons-material/HighlightOffRounded"
 
 const InventoryPage = () => {
@@ -10,9 +11,12 @@ const InventoryPage = () => {
     <div className="h-full border-l-[1px] border-l-neutral-700">
       <div className="flex">
         {open && (
-          <div className=" h-[248px] w-[276px] bg-neutral-780 transition-all duration-500 ease-in">
+          <motion.div
+            transition={{ duration: 5, ease: "easeOut", delay: 0.4 }}
+            className=" h-[248px] w-[276px] bg-neutral-780 transition-all duration-500 ease-in"
+          >
             Filter
-          </div>
+          </motion.div>
         )}
         <Box
           className="grid h-[40px] w-[40px] cursor-pointer items-center justify-items-center rounded-r-lg bg-error-main"

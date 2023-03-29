658d5e6b2c4eac03839de22efb5451334306e714
Kannika M <m@kos.co.th>
2023-03-29 18:57:47 +0700

    fix: set default for hex value

--- src/constants/defaultValues.ts
+++ src/constants/defaultValues.ts
@@ -1,5 +1,6 @@
 import { ITokenContract } from "@feature/contract/containers/hooks/useContractVaultBinance"
 import { IErrorMessage } from "@interfaces/IErrorMessage"
+import { BigNumber } from "ethers"
 
 export const DEFAULT_STATUS_WALLET: IErrorMessage = {
   responseStatus: false,
@@ -13,10 +14,12 @@ export const DEFAULT_TOKEN_INFO: ITokenContract = {
   address: "",
   balanceWallet: {
     digit: 0,
-    text: ""
+    text: "",
+    hex: BigNumber.from(0)
   },
   balanceVault: {
     digit: 0,
-    text: ""
+    text: "",
+    hex: BigNumber.from(0)
   }
 }

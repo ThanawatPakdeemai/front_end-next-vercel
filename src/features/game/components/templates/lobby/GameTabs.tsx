42ce2c07ddbd4572be78df2b7f0f1452fefbccac
Kannika M <m@kos.co.th>
2023-04-03 14:40:14 +0700

    feat: updated env

--- src/features/game/components/templates/lobby/GameTabs.tsx
+++ src/features/game/components/templates/lobby/GameTabs.tsx
@@ -12,16 +12,19 @@ import { IGetType } from "@feature/game/interfaces/IGameService"
 import useGameWhatsNew from "@feature/game/containers/hooks/useGameWhatsNew"
 import useGameOverview from "@feature/game/containers/hooks/useGameOverview"
 import AboutGame from "@components/organisms/AboutGame"
-import WhatsNewBody from "../../molecules/WhatsNewBody"
-import HowToPlayBody from "../../molecules/HowToPlayBody"
-import GameItemsBody from "../../molecules/GameItemsBody"
+import useGameStore from "@stores/game"
+import _ from "lodash"
+import GameItemsBody from "@feature/game/components/molecules/GameItemsBody"
+import HowToPlayBody from "@feature/game/components/molecules/HowToPlayBody"
+import WhatsNewBody from "@feature/game/components/molecules/WhatsNewBody"
 
 interface IProps {
   gameType: IGetType
   gameId: string
 }
 const GameTabs = ({ gameType, gameId }: IProps) => {
-  const { hydrated } = useGlobal()
+  const { hydrated, getTypeGamePathFolder } = useGlobal()
+  const { data: gameData } = useGameStore()
   const { t } = useTranslation()
   const { handleChangeTab } = useTab()
   const { tabValue, setTabValue } = useTabContext()
@@ -30,15 +33,38 @@ const GameTabs = ({ gameType, gameId }: IProps) => {
   const { singleVersion, gameHowToPlay, gameItems, gameDescription } =
     useGameOverview(gameId, gameType)
 
-  /**
-   * @description Tab Content Partner Game
-   */
-  const GAME_TAB_CONTENT: {
+  const _gameType: {
     id: string
     label: string
     icon?: React.ReactNode
     component?: React.ReactNode
-  }[] = [
+  } =
+    gameData &&
+    (getTypeGamePathFolder(gameData) === "free-to-play" ||
+      getTypeGamePathFolder(gameData) === "story-mode")
+      ? ({} as {
+          id: string
+          label: string
+          icon?: React.ReactNode
+          component?: React.ReactNode
+        })
+      : {
+          id: "game-items",
+          label: t("game_items"),
+          icon: <IDiamond stroke="#70727B" />,
+          component: <GameItemsBody gameItems={gameItems} />
+        }
+
+  /**
+   * @description Tab Content Partner Game
+   */
+  const GAME_TAB_CONTENT:
+    | {
+        id: string
+        label: string
+        icon?: React.ReactNode
+        component?: React.ReactNode
+      }[] = [
     {
       id: "about-us",
       label: t("game_details"),
@@ -51,12 +77,7 @@ const GameTabs = ({ gameType, gameId }: IProps) => {
       icon: <HowToPlayIcon stroke="#70727B" />,
       component: <HowToPlayBody text={gameHowToPlay} />
     },
-    {
-      id: "game-items",
-      label: t("game_items"),
-      icon: <IDiamond stroke="#70727B" />,
-      component: <GameItemsBody gameItems={gameItems} />
-    },
+    _gameType,
     {
       id: "whats-new",
       label: t("whats_new"),
@@ -88,41 +109,44 @@ const GameTabs = ({ gameType, gameId }: IProps) => {
     <Box className="relative h-full">
       <div className="absolute top-[-80px] left-[-30px] flex w-full flex-wrap overflow-hidden sm:w-auto">
         <div className="flex w-full items-center justify-between gap-2 rounded-xl bg-neutral-700 bg-opacity-40 px-1 capitalize sm:w-auto">
-          {GAME_TAB_CONTENT.map((item) => (
-            <Tab
-              sx={{
-                padding: "0!important"
-              }}
-              onClick={() => handleChangeTab(item.id)}
-              label={
-                <Chip
-                  label={item.label}
-                  size="medium"
-                  className={`bg-nuetral-800 cursor-pointer font-bold capitalize hover:text-white-default ${
-                    item.id === tabValue ? " !text-white-default" : ""
-                  }`}
-                  sx={item.id === tabValue ? { path: { stroke: "#fff" } } : {}}
-                  icon={item.icon as React.ReactElement}
-                />
-              }
-              value={item.id}
-              key={item.id}
-            />
-          ))}
+          {GAME_TAB_CONTENT.map((item) =>
+            _.isEmpty(item) ? null : (
+              <Tab
+                sx={{
+                  padding: "0!important"
+                }}
+                onClick={() => handleChangeTab(item.id)}
+                label={
+                  <Chip
+                    label={item.label}
+                    size="medium"
+                    className={`bg-nuetral-800 cursor-pointer font-bold capitalize hover:text-white-default ${
+                      item.id === tabValue ? " !text-white-default" : ""
+                    }`}
+                    sx={
+                      item.id === tabValue ? { path: { stroke: "#fff" } } : {}
+                    }
+                    icon={item.icon as React.ReactElement}
+                  />
+                }
+                value={item.id}
+                key={item.id}
+              />
+            )
+          )}
         </div>
       </div>
       <PanelContent height="h-[500px]">
         <div className="p-4">
-          {GAME_TAB_CONTENT.map(
-            (item) =>
-              item.id === tabValue && (
-                <Box
-                  role="tabpanel"
-                  key={item.id}
-                >
-                  {item.component}
-                </Box>
-              )
+          {GAME_TAB_CONTENT.map((item) =>
+            item.id === tabValue ? (
+              <Box
+                role="tabpanel"
+                key={item.id}
+              >
+                {item.component}
+              </Box>
+            ) : null
           )}
         </div>
       </PanelContent>

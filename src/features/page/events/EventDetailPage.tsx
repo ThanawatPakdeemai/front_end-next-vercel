5dd02d68e83be35bc1a9a803f122dcd77c764728
Kannika M <m@kos.co.th>
2023-04-06 18:37:32 +0700

    style: improved css

--- src/features/page/events/EventDetailPage.tsx
+++ src/features/page/events/EventDetailPage.tsx
@@ -261,14 +261,14 @@ const EventDetailPage = ({ _eventId }: IEventDetailProps) => {
             />
           }
           component2={
-            <FullWidthContent
-              sxCustomStyled={{
+            <Box
+              sx={{
                 "&.container": {
                   maxWidth: "100%!important"
                 }
               }}
             >
-              <div className="rounded-md border-[1px] border-neutral-700 border-opacity-80 bg-neutral-780 p-4 font-neue-machina-bold md:my-2 md:w-4/6 md:px-16 md:py-8 md:text-center md:text-base">
+              <div className="w-full rounded-md border-[1px] border-neutral-700 border-opacity-80 bg-neutral-780 p-4 font-neue-machina-bold md:my-2 md:px-16 md:py-8 md:text-center md:text-base">
                 COMMENT AND HASHTAG #YOUR_USERNAME #NAKARUNNER IN YOUR TWITTER
                 SHARING NAKA RUNNER CHALLENGE POST TO MAKE SURE YOU ATTEND THE
                 EVENT [FIRST 102 PEOPLE TO SCORE OVER 250,000 WILL WIN THE
@@ -283,7 +283,7 @@ const EventDetailPage = ({ _eventId }: IEventDetailProps) => {
                 </Typography>
               </div>
               {renderPlayerContent()}
-            </FullWidthContent>
+            </Box>
           }
         />
       ) : (

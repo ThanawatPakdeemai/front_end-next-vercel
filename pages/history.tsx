cfb3baab46bab4f5377722d068fa7bb9a4defa2b
SirawitPaiKOS <pai@kos.co.th>
2023-04-21 17:53:54 +0700

    fix : Mobile Site - Menu (Fixed bottom) - Notification - Item Rewards - History Game play

--- pages/history.tsx
+++ pages/history.tsx
@@ -2,6 +2,7 @@ import { ReactElement } from "react"
 import { serverSideTranslations } from "next-i18next/serverSideTranslations"
 import { GAME_PLAY_HISTORY } from "@configs/crumb"
 import dynamic from "next/dynamic"
+import { BrowserView, MobileView } from "react-device-detect"
 
 const ProfileLayout = dynamic(
   () => import("@components/templates/ProfileLayout"),
@@ -17,10 +18,25 @@ const HistoryTable = dynamic(
     ssr: false
   }
 )
+const HistoryTableMobile = dynamic(
+  () =>
+    import(
+      "@src/mobile/features/history/components/organisms/HistoryTableMobile"
+    ),
+  {
+    suspense: true,
+    ssr: false
+  }
+)
 
 const HistoryPage = () => (
   <article className="h-full w-full">
-    <HistoryTable />
+    <BrowserView>
+      <HistoryTable />
+    </BrowserView>
+    <MobileView>
+      <HistoryTableMobile />
+    </MobileView>
   </article>
 )
 

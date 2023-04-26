600ecd657d8658152cdbe5e1a980e02c376f8eb6
yui <127318775+jyu1q@users.noreply.github.com>
2023-04-26 15:44:19 +0700

    fix: get room multi

--- src/mobile/features/game/components/templates/multi/MultiRoom.tsx
+++ src/mobile/features/game/components/templates/multi/MultiRoom.tsx
@@ -1,24 +1,30 @@
-import useRoomSingle from "@feature/game/containers/hooks/useRoomSingle"
+import useRoomMulti from "@feature/game/containers/hooks/useRoomMulti"
+import ModalCreateRoom from "@feature/rooms/components/molecules/ModalCreateRoom"
 import ListJoinGame from "../../molecules/ListJoinGame"
 
 const MultiRoom = () => {
-  const { roomData, gameData, itemSelected, handleJoinRoom } = useRoomSingle()
+  const { dataRoom, data, handleJoinRoom, itemSelected } = useRoomMulti()
   return (
     <>
-      {roomData &&
-        gameData &&
-        roomData?.map((item) => (
-          <ListJoinGame
-            key={item._id}
-            image={gameData.image_room}
-            name={gameData.game_type}
-            desc={gameData.name}
-            onClick={() => handleJoinRoom(item)}
-            textChip={`#${item?.room_number?.toString()}`}
-            descChip1={`${itemSelected?.name} / ${itemSelected?.item_size}`}
-            descChip2={`${item.amount_played} / ${item.max_players}`}
-          />
-        ))}
+      {data && (
+        <>
+          <ModalCreateRoom gameData={data} />
+          {dataRoom &&
+            dataRoom &&
+            dataRoom?.map((item) => (
+              <ListJoinGame
+                key={item._id}
+                image={data.image_room}
+                name={data.game_type}
+                desc={data.name}
+                onClick={() => handleJoinRoom(item)}
+                textChip={`#${item?.room_number?.toString()}`}
+                descChip1={`${itemSelected?.name} / ${itemSelected?.item_size}`}
+                descChip2={`${item.amount_played} / ${item.max_players}`}
+              />
+            ))}
+        </>
+      )}
     </>
   )
 }

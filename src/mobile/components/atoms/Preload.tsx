c82062ccc10d448b7b46bafd8bede2f006800d64
yui <127318775+jyu1q@users.noreply.github.com>
2023-04-27 13:37:34 +0700

    fix: add memo when export preload

--- src/mobile/components/atoms/Preload.tsx
+++ src/mobile/components/atoms/Preload.tsx
@@ -1,28 +1,22 @@
 import LogoNakaIcon from "@components/icons/LogoNakaIcon"
 import INaka from "@components/icons/Naka"
-import { useMemo } from "react"
+import { memo } from "react"
 
-const Preload = () => {
-  const load = useMemo(
-    () => (
-      <div className="flex h-[100vh] flex-col items-center justify-center ">
-        <div className="flex h-[60%] items-center">
-          <INaka
-            width={184}
-            height={84}
-          />
-        </div>
-        <div>
-          <LogoNakaIcon
-            fill="!fill-neutral-700"
-            width={225}
-            hover={false}
-          />
-        </div>
-      </div>
-    ),
-    []
-  )
-  return load
-}
-export default Preload
+const Preload = () => (
+  <div className="flex h-[100vh] flex-col items-center justify-center ">
+    <div className="flex h-[60%] items-center">
+      <INaka
+        width={184}
+        height={84}
+      />
+    </div>
+    <div>
+      <LogoNakaIcon
+        fill="!fill-neutral-700"
+        width={225}
+        hover={false}
+      />
+    </div>
+  </div>
+)
+export default memo(Preload)

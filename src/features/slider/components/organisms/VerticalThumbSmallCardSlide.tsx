774d0bc5c20e35fb71d0b185e1fd11b0bc056061
F.Boy <>
2023-04-28 00:13:52 +0700

    fix: design mobile of game detail

--- src/features/slider/components/organisms/VerticalThumbSmallCardSlide.tsx
+++ src/features/slider/components/organisms/VerticalThumbSmallCardSlide.tsx
@@ -3,13 +3,18 @@ import { IVerticalThumbCardSlideProps } from "@feature/slider/interfaces/ISlides
 import { CardMedia } from "@mui/material"
 import PlayArrowIcon from "@mui/icons-material/PlayArrow"
 import { ImageCustom } from "@components/atoms/image/Image"
+import { isMobile } from "react-device-detect"
 
 const VerticalThumbSmallCardSlide = ({
   item
 }: IVerticalThumbCardSlideProps) => (
   <div className="verticalSmallThumb-slide__item relative my-[2px]">
-    <div className="verticalSmallThumb-slide__item__image h-[70px] w-[70px] cursor-pointer transition-all hover:opacity-70">
-      {item.type === "video" && (
+    <div
+      className={`verticalSmallThumb-slide__item__image cursor-pointer transition-all hover:opacity-70 ${
+        isMobile ? "mx-auto my-0 h-[11.64rem] w-auto" : "h-[70px] w-[70px]"
+      }`}
+    >
+      {item.type === "video" && !isMobile && (
         <div className="absolute flex h-full w-full items-center justify-center">
           <span className="flex h-8 w-8 items-center justify-center rounded bg-neutral-900/60">
             <PlayArrowIcon className="text-base text-neutral-300" />
@@ -18,12 +23,14 @@ const VerticalThumbSmallCardSlide = ({
       )}
       {item.type === "video" ? (
         <CardMedia
-          className="card-media block h-[70px] w-[70px] overflow-hidden rounded-sm border-2 border-neutral-700 object-cover object-center transition-all"
+          className={`card-media block overflow-hidden rounded-sm border-2 border-neutral-700 object-cover object-center transition-all ${
+            isMobile ? "mx-auto my-0 h-[11.64rem] w-auto" : "h-[70px] w-[70px]"
+          }`}
           component={item.type === "video" ? "video" : "img"}
           alt="Slide"
           src={item.src}
           autoPlay={false}
-          controls={false}
+          controls={!!isMobile}
         />
       ) : (
         <ImageCustom

198d67bf258bb3fcd8bde7c86330f329faeda8ea
yui <127318775+jyu1q@users.noreply.github.com>
2023-04-27 12:54:34 +0700

    fix: css moblie

--- src/mobile/components/atoms/Preload.tsx
+++ src/mobile/components/atoms/Preload.tsx
@@ -3,18 +3,20 @@ import INaka from "@components/icons/Naka"
 
 const Preload = () => (
   <>
-    <div className="flex h-[80vh] flex-row items-center justify-center ">
-      <INaka
-        width={184}
-        height={84}
-      />
-    </div>
-    <div className="flex flex-row items-center justify-center ">
-      <LogoNakaIcon
-        fill="!fill-neutral-700"
-        width={225}
-        hover={false}
-      />
+    <div className="flex h-[100vh] flex-col items-center justify-center ">
+      <div className="flex h-[60%] items-center">
+        <INaka
+          width={184}
+          height={84}
+        />
+      </div>
+      <div>
+        <LogoNakaIcon
+          fill="!fill-neutral-700"
+          width={225}
+          hover={false}
+        />
+      </div>
     </div>
   </>
 )

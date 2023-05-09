996b7f51d1e22418d9f78f91f2bde839da048b52
F.Boy <>
2023-05-09 10:40:58 +0700

    fix: update detect component mobile

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -7,7 +7,7 @@ import { RightMenu } from "@components/molecules/rightMenu"
 import useGlobal from "@hooks/useGlobal"
 import RightMenuDeveloper from "@components/molecules/rightMenu/RightMenuDeveloper"
 import HeadMenuMobile from "@src/mobile/headerMenu/HeadMenuMobile"
-import { BrowserView, MobileView } from "react-device-detect"
+import { MobileView } from "react-device-detect"
 import HeadProfileMobile from "@src/mobile/headerMenu/HeadProfileMobile"
 import CONFIGS from "@configs/index"
 
@@ -21,25 +21,26 @@ const Header = () => {
 
   return (
     <div className="header-wrapper">
-      <BrowserView>
-        {showHeadPrice && <HeadPrice />}
-        <header className="header relative top-10 z-[999] lg:sticky">
-          <Box
-            component="div"
-            className="flex flex-wrap items-center justify-between md:my-10 xl:flex-nowrap"
-          >
-            <HeadLogo />
-            {showHeadMenu && <HeadMenu />}
-            {showRightMenu && <RightMenu />}
-            {showRightMenuDeveloper && <RightMenuDeveloper />}
-          </Box>
-        </header>
-      </BrowserView>
-      {CONFIGS.DISPLAY_MOBILE_MODE === "true" && (
+      {CONFIGS.DISPLAY_MOBILE_MODE === "true" ? (
         <MobileView>
           <HeadProfileMobile />
           <HeadMenuMobile />
         </MobileView>
+      ) : (
+        <>
+          {showHeadPrice && <HeadPrice />}
+          <header className="header relative top-10 z-[999] lg:sticky">
+            <Box
+              component="div"
+              className="flex flex-wrap items-center justify-between md:my-10 xl:flex-nowrap"
+            >
+              <HeadLogo />
+              {showHeadMenu && <HeadMenu />}
+              {showRightMenu && <RightMenu />}
+              {showRightMenuDeveloper && <RightMenuDeveloper />}
+            </Box>
+          </header>
+        </>
       )}
     </div>
   )

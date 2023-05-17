aceb4fdd57cc00bb4d675afbec96a2e91ccdf365
F.Boy <>
2023-05-17 19:03:28 +0700

    fix: edit text and css

--- src/components/molecules/gameSlide/GameCarouselHeader.tsx
+++ src/components/molecules/gameSlide/GameCarouselHeader.tsx
@@ -134,7 +134,7 @@ const GameCarouselHeader = ({
         </motion.div>
       )}
 
-      <div className="flex h-full w-full flex-wrap items-center justify-between gap-[5px] sm:flex-nowrap">
+      <div className="flex h-full w-full flex-wrap items-center justify-between gap-[5px] overflow-x-auto sm:flex-nowrap">
         {menu && (
           <div
             className={`relative flex h-full w-fit flex-wrap items-center justify-between rounded-xl border-2 border-neutral-800 bg-neutral-900 bg-opacity-40 p-[5px] text-[10px] capitalize backdrop-blur-[25px] sm:flex-nowrap lg:flex-none ${

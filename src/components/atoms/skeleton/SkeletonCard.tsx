13b34ffc77be98edf119936e94e76946fdb8318c
Kannika M <m@kos.co.th>
2023-05-23 11:44:45 +0700

    fix: improved function display reward

--- src/components/atoms/skeleton/SkeletonCard.tsx
+++ src/components/atoms/skeleton/SkeletonCard.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@mui/material"
 import React, { memo } from "react"
 
 const SkeletonCard = () => (
-  <div className="relative h-full w-full">
+  <div className="relative h-full w-full flex-1">
     <Skeleton
       variant="rectangular"
       width="100%"

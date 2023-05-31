cf4dc551ef3602df5df23f462cdb2cad0df180fd
F.Boy <>
2023-05-31 18:30:04 +0700

    feat: redesign page login  of mobile-site

--- src/mobile/components/templates/SignInLayout.tsx
+++ src/mobile/components/templates/SignInLayout.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react"
-import { Box, Button, Stack, Typography } from "@mui/material"
+import React from "react"
+import { Box, Button, Divider, Typography } from "@mui/material"
 import {
   getAuth,
   GoogleAuthProvider,
@@ -8,21 +8,17 @@ import {
 } from "firebase/auth"
 import { getApps, initializeApp } from "@firebase/app"
 import CardNoReward from "@feature/game/containers/components/atoms/CardNoReward"
-import DraftsOutlinedIcon from "@mui/icons-material/DraftsOutlined"
-import FacebookIcon from "@components/icons/SocialIcon/FacebookIcon"
 import TwitterIcon from "@components/icons/SocialIcon/TwitterIcon"
-import GoogleIcon from "@components/icons/SocialIcon/GoogleIcon"
-import { ModalCustom } from "@components/molecules/Modal/ModalCustom"
-import ModalHeader from "@components/molecules/Modal/ModalHeader"
-import FormLogin from "@feature/authentication/components/FormLogin"
-import useLoginTypeStore from "@stores/loginTypes"
-import FacebookLogin from "react-facebook-login"
+// import useLoginTypeStore from "@stores/loginTypes"
+// import FacebookLogin from "react-facebook-login"
 import useLoginProvider from "@feature/authentication/containers/hooks/useLoginProvider"
 import { useToast } from "@feature/toast/containers"
-import { IProfileFaceBook } from "@src/types/profile"
+// import { IProfileFaceBook } from "@src/types/profile"
 import { IError } from "@src/types/contract"
 import { MESSAGES } from "@constants/messages"
 import LogoNakaBigIcon from "@components/icons/LogoNakaBigIcon"
+import GoogleColorIcon from "@components/icons/SocialIcon/GoogleColorIcon"
+import FacebookColorIcon from "@components/icons/SocialIcon/FacebookColorIcon"
 
 const SignInLayout = () => {
   const { mutateLoginProvider } = useLoginProvider()
@@ -41,44 +37,46 @@ const SignInLayout = () => {
     initializeApp(firebaseConfig)
   }
 
+  /* TODO Boy login with facebook */
+
   const auth = getAuth()
 
-  const {
-    getClickLoginFacebook: toggleFacebookLogin,
-    setClickLoginFacebook: setToggleFacebookLogin
-  } = useLoginTypeStore()
+  // const {
+  //   getClickLoginFacebook: toggleFacebookLogin,
+  //   setClickLoginFacebook: setToggleFacebookLogin
+  // } = useLoginTypeStore()
 
   const { errorToast, successToast } = useToast()
 
-  const [open, setOpen] = useState<boolean>(false)
+  // const [open, setOpen] = useState<boolean>(false)
 
-  const handleOpen = () => setOpen(true)
-  const handleClose = () => setOpen(false)
+  // const handleOpen = () => setOpen(true)
+  // const handleClose = () => setOpen(false)
 
-  const facebookLogin = async (response: IProfileFaceBook) => {
-    if (
-      response.email !== null &&
-      response.email !== undefined &&
-      response.userID !== null &&
-      response.userID !== undefined
-    ) {
-      mutateLoginProvider({
-        _email: response.email,
-        _provider: "facebook",
-        _prevPath: "/",
-        _providerUUID: response.userID,
-        _referral: ""
-      })
-        .then((_res) => {
-          if (_res) {
-            successToast(MESSAGES.logged_in_successfully)
-          }
-        })
-        .catch((_error: IError) => {
-          errorToast(MESSAGES.logged_in_unsuccessfully || _error.message)
-        })
-    }
-  }
+  // const facebookLogin = async (response: IProfileFaceBook) => {
+  //   if (
+  //     response.email !== null &&
+  //     response.email !== undefined &&
+  //     response.userID !== null &&
+  //     response.userID !== undefined
+  //   ) {
+  //     mutateLoginProvider({
+  //       _email: response.email,
+  //       _provider: "facebook",
+  //       _prevPath: "/",
+  //       _providerUUID: response.userID,
+  //       _referral: ""
+  //     })
+  //       .then((_res) => {
+  //         if (_res) {
+  //           successToast(MESSAGES.logged_in_successfully)
+  //         }
+  //       })
+  //       .catch((_error: IError) => {
+  //         errorToast(MESSAGES.logged_in_unsuccessfully || _error.message)
+  //       })
+  //   }
+  // }
 
   const twitterLogin = async () => {
     const provider = new TwitterAuthProvider()
@@ -160,81 +158,73 @@ const SignInLayout = () => {
       >
         <Box
           component="div"
-          className="flex justify-center"
+          className="mb-20 flex justify-center"
         >
           <LogoNakaBigIcon />
         </Box>
         <Typography className="my-8 text-center font-urbanist text-3xl font-bold uppercase text-red-card">
           Welcome Back
         </Typography>
-        <div>
-          <Button
-            variant="contained"
-            className="mb-[1.125rem] h-[50px] w-[293px] rounded-lg border border-solid border-neutral-700
-!bg-neutral-800 text-xs uppercase hover:border-secondary-main"
-            onClick={() => setToggleFacebookLogin(true)}
-          >
-            <div className="flex items-center font-neue-machina text-sm font-bold">
-              <span className="absolute left-[15px] top-2.5">
-                {toggleFacebookLogin ? (
-                  <FacebookLogin
-                    appId={`${process.env.NEXT_PUBLIC_FACEBOOK_APPID}`}
-                    autoLoad
-                    fields="name,email,picture"
-                    callback={facebookLogin}
-                    cssClass="my-facebook-button-class"
-                    textButton={null}
-                    icon={<FacebookIcon />}
-                  />
-                ) : (
-                  <FacebookIcon />
-                )}
-              </span>
-              sign in with Facebook
-            </div>
-          </Button>
-        </div>
-        <div>
-          <Button
-            variant="contained"
-            className="mb-[1.125rem] h-[50px] w-[293px] rounded-lg border border-solid border-neutral-700
-!bg-neutral-800 text-xs uppercase hover:border-secondary-main"
-            onClick={googleLogin}
-          >
-            <div className="flex items-center font-neue-machina text-sm font-bold">
-              <span className="absolute left-[15px]">
-                <GoogleIcon />
-              </span>
-              sign in with Google
-            </div>
-          </Button>
-        </div>
-        <div>
-          <Button
-            variant="contained"
-            className="mb-[2.813rem] h-[50px] w-[293px] rounded-lg border border-solid border-neutral-700
-!bg-neutral-800 text-xs uppercase hover:border-secondary-main"
-            onClick={twitterLogin}
-          >
-            <div className="flex items-center font-neue-machina text-sm font-bold">
-              <span className="absolute left-[15px]">
-                <TwitterIcon />
-              </span>
-              sign in with Twitter
-            </div>
-          </Button>
-        </div>
         <Box component="div">
           <Button
             variant="contained"
-            className="mb-[1.125rem] h-[50px] w-[293px] rounded-lg border border-solid border-neutral-700
-!bg-neutral-800 text-xs hover:border-secondary-main"
-            onClick={handleOpen}
+            className="mb-[1.125rem] h-[50px] w-[293px] rounded-2xl border border-solid border-neutral-690 !bg-neutral-800"
+            onClick={googleLogin}
           >
-            <div className="flex items-center font-neue-machina text-sm font-bold">
-              <span className="absolute left-[15px]">
-                <DraftsOutlinedIcon />
+            <div className="flex items-center font-urbanist text-base font-medium">
+              <span className="pr-2">
+                <FacebookColorIcon />
               </span>
+              <span>Sign in with Facebook</span>
+            </div>
+          </Button>
+        </Box>
+        <Box component="div">
+          <Button
+            variant="contained"
+            className="mb-[1.125rem] h-[50px] w-[293px] rounded-2xl border border-solid border-neutral-690 !bg-neutral-800"
+            onClick={googleLogin}
+          >
+            <div className="flex items-center font-urbanist text-base font-medium">
+              <span className="pr-2">
+                <GoogleColorIcon />
+              </span>
+              <span>Sign in with Google</span>
+            </div>
+          </Button>
+        </Box>
+        <Box component="div">
+          <Button
+            variant="contained"
+            className="h-[50px] w-[293px] rounded-2xl border border-solid border-neutral-690 !bg-neutral-800"
+            onClick={twitterLogin}
+          >
+            <div className="flex items-center font-urbanist text-base font-medium">
+              <span className="pr-2">
+                <TwitterIcon fill="#1D9BF0" />
+              </span>
+              <span>Sign in with Twitter</span>
+            </div>
+          </Button>
+        </Box>
+        <Box
+          component="div"
+          className="py-4"
+        >
+          <Divider
+            sx={{ color: "#fff" }}
+            className="font-urbanist font-medium"
+          >
+            or
+          </Divider>
+        </Box>
+        <Box component="div">
+          <Button
+            variant="contained"
+            className="mb-6 h-[50px] w-[293px] rounded-bl-3xl border border-solid border-error-100 !bg-error-100"
+            // onClick={handleOpen}
+          >
+            <div className="flex items-center font-urbanist text-base font-bold">
               Sign in with Email
             </div>
           </Button>
@@ -243,34 +233,16 @@ const SignInLayout = () => {
           component="div"
           className="flex justify-center text-center"
         >
-          <p className="text-white pr-2 text-sm font-normal">
+          <p className="pr-2 text-sm font-normal text-[#fff]">
             Don’t have an account?
           </p>
-          <p className="text-sm font-normal text-yellow-100">Sign up</p>
+          <p className="text-sm font-normal text-warning-100">Sign up</p>
         </Box>
         <CardNoReward
-          className="!rounded-none !border-none !bg-transparent"
+          className="!rounded-none !border-none !bg-transparent !p-5"
           showIconTM={false}
         />
       </Box>
-      <ModalCustom
-        open={open}
-        onClose={handleClose}
-        className="w-full gap-3 rounded-[34px] p-[10px] md:w-auto"
-        width="auto"
-      >
-        <Stack
-          spacing={3}
-          className="md:p-5"
-        >
-          <ModalHeader
-            handleClose={handleClose}
-            title="Login"
-          />
-
-          <FormLogin />
-        </Stack>
-      </ModalCustom>
     </>
   )
 }

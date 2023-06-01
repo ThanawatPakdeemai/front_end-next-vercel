03dfa87b947ed3c98f8000638d314f0beb13ea7e
Dodo_KOSdesign <109782498+supakorn672@users.noreply.github.com>
2023-06-01 11:39:02 +0700

    fix:remove console.log

--- src/features/marketplace/components/molecules/FilterBox.tsx
+++ src/features/marketplace/components/molecules/FilterBox.tsx
@@ -274,8 +274,6 @@ const FilterBox = () => {
     return _date
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [search])
-  // eslint-disable-next-line no-console
-  console.log()
   return (
     <div className="grid w-52 gap-3 ">
       <section>

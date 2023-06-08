f791410b0665916a1d2500aa0ebd897a54870c89
Kannika M <m@kos.co.th>
2023-06-08 10:59:00 +0700

    feat: hide menu

--- src/components/atoms/select/SelectNaka.tsx
+++ src/components/atoms/select/SelectNaka.tsx
@@ -88,7 +88,10 @@ const SelectNaka = ({
                   </div>
                 </div>
                 <div className="flex items-center justify-between bg-primary-main pt-2 ">
-                  <div className="flex w-full items-center  justify-between rounded-default bg-neutral-700 p-[3px]">
+                  <Box
+                    component={"div"}
+                    className="menu-list flex w-full  items-center justify-between rounded-default bg-neutral-700 p-[3px]"
+                  >
                     <MenuList className="w-full !p-[3px]">
                       {options.map((option) => (
                         <MenuItem
@@ -104,6 +107,15 @@ const SelectNaka = ({
                               router.push(option.link)
                             }
                           }}
+                          sx={
+                            option.value === "Become Developer"
+                              ? {
+                                  "&.MuiMenuItem-root": {
+                                    display: "none"
+                                  }
+                                }
+                              : {}
+                          }
                         >
                           <Link
                             href={option.link ?? ""}
@@ -135,7 +147,7 @@ const SelectNaka = ({
                       ))}
                     </MenuList>
                     <div className="px-2 py-1">{imageSelectd}</div>
-                  </div>
+                  </Box>
                 </div>
               </div>
             </Popover>

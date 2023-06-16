315a8acf0e8993c7f96091166b15360004664191
Buchit Baltip <tiger@kos.co.th>
2023-06-16 18:13:06 +0700

    fix: eror

--- src/components/molecules/profile/ProfileContent.tsx
+++ src/components/molecules/profile/ProfileContent.tsx
@@ -96,7 +96,7 @@ const ProfileContent = () => {
   const { linkTelegramData } = useLinkToTelegram(idPlayer, telegramId)
 
   useEffect(() => {
-    if (telegramParams !== undefined || null) {
+    if (telegramParams !== undefined || telegramParams !== null) {
       // eslint-disable-next-line no-console
       console.log("telegram_", telegramParams.id)
       // eslint-disable-next-line no-console

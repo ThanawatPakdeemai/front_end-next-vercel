921af2fd137405a38884f13cd8123538d316ecb2
F.Boy <>
2023-06-19 13:50:39 +0700

    fix: merge code from develop

--- src/mobile/components/organisms/modal/CreateAccountModal.tsx
+++ src/mobile/components/organisms/modal/CreateAccountModal.tsx
@@ -39,7 +39,6 @@ const CreateAccountModal = ({
     onClickGetCode,
     isNumber,
     isCharacters,
-    watch,
     handleClickShowPassword,
     showConfirmPassword,
     handleClickShowConfirmPassword

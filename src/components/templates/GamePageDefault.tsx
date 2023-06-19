86a4770cb5d2bb07ae69a6eae7ac0599ecd57e8f
Kannika M <m@kos.co.th>
2023-06-19 19:21:33 +0700

    fix: bug loading because of favourite game function

--- src/components/templates/GamePageDefault.tsx
+++ src/components/templates/GamePageDefault.tsx
@@ -1,30 +1,19 @@
 import React, { useEffect, useState } from "react"
-import ShineIcon from "@components/icons/ShineIcon"
 import Banners from "@components/molecules/Banners"
 import BannerSingle from "@components/molecules/BannerSingle"
 import StatisticGameDetail from "@components/molecules/statistic/StatisticGameDetail"
-import Tagline from "@components/molecules/tagline/Tagline"
 import Footer from "@components/organisms/Footer"
 import Header from "@components/organisms/Header"
 import { IGame } from "@feature/game/interfaces/IGameService"
 import { IPartnerGameData } from "@feature/game/interfaces/IPartnerGame"
-import useGlobal, { isMobile } from "@hooks/useGlobal"
+import useGlobal from "@hooks/useGlobal"
 import useGetStatisticsGameById from "@feature/game/containers/hooks/useGetStatisticsGameById"
 import TopPlayer from "@feature/ranking/components/template/TopPlayer"
 import useGameStore from "@stores/game"
 import Howto from "@components/molecules/HowToPlay"
 import { Box } from "@mui/material"
-import { useTranslation } from "react-i18next"
 import useBuyGameItemController from "@feature/buyItem/containers/hooks/useBuyGameItemController"
 import LikeNoLobby from "@components/molecules/LikeNoLobby"
-import InvestIcon from "@components/icons/Stats/InvestIcon"
-import PlayersIcon from "@components/icons/Stats/PlayersIcon"
-import RewardIcon from "@components/icons/Stats/RewardIcon"
-import StatWithIcon from "@components/molecules/statistic/StatWithIcon"
-import StatsDetail from "@components/molecules/statistic/StatsDetail"
-import BankIcon from "@components/icons/BankIcon"
-import ControllerIcon from "@components/icons/ControllerIcon"
-import StatEstimatedProfit from "@components/molecules/statistic/StatEstimatedProfit"
 import IconArrowLeft from "@components/icons/arrowLeftIcon"
 import IconArrowRight from "@components/icons/arrowRightIcon"
 import useGameOverview from "@feature/game/containers/hooks/useGameOverview"
@@ -49,7 +38,6 @@ const GamePageDefault = ({
   const gamePartnerData = useGameStore((state) => state.dataGamePartner)
   const [gameData, setGameData] = useState<IGame | IPartnerGameData>()
   const { statsGameById } = useGetStatisticsGameById()
-  const { t } = useTranslation()
   const router = useRouter()
   const isReward =
     router.pathname &&
@@ -69,7 +57,6 @@ const GamePageDefault = ({
    * @returns
    */
   const renderStatistic = () => {
-    const shop = true
     const buttonArrow =
       "flex flex-1 items-center justify-center p-[0_10px_0_15px] h-full"
 
@@ -83,176 +70,52 @@ const GamePageDefault = ({
       default:
         return (
           <div className="game-page-default w-full">
-            {isMobile ? (
-              <>
-                <Box component="section">
-                  <Tagline
-                    bgColor="bg-neutral-800"
-                    textColor="text-neutral-500 font-bold"
-                    text={t("game_page_tagline_desc")}
-                    icon={<ShineIcon />}
-                    show={shop}
-                  />
-                  <div className="mb-12 grid grid-cols-2 gap-2">
-                    <LikeNoLobby
-                      imgSrc={
-                        gameData && "image_category_list" in gameData
-                          ? gameData.image_category_list
-                          : ""
-                      }
-                      value={78.34}
-                    />
-                    <div className="flex flex-col gap-2 ">
-                      <div className=" flex flex-auto flex-col justify-between gap-2 ">
-                        <StatWithIcon
-                          icon={
-                            <PlayersIcon className="h-[60px] w-[60px] rotate-0" />
-                          }
-                          className="bg-error-main"
-                          textColor="text-error-main"
-                          title={t("players_today")}
-                          amount={statsGameById?.data.player_number || 0}
-                          unit={t("people")}
-                        />
-                        <StatWithIcon
-                          icon={
-                            <InvestIcon className="h-[60px] w-[60px] rotate-0" />
-                          }
-                          className="bg-secondary-main"
-                          textColor="text-secondary-main"
-                          title={t("invest_today")}
-                          amount={statsGameById?.data.invest || 0}
-                          unit="naka"
-                        />
-                        <StatWithIcon
-                          icon={
-                            <RewardIcon className="h-[60px] w-[60px] rotate-0" />
-                          }
-                          className="bg-varidian-default"
-                          textColor="text-varidian-default"
-                          title={t("reward_today")}
-                          amount={statsGameById?.data.reward_naka || 0}
-                          unit="naka"
-                        />
-                      </div>
-                    </div>
-                    <div className="col-span-2 my-4 flex w-full flex-auto grid-cols-3 flex-col justify-evenly gap-2 ">
-                      <div className="flex w-full flex-auto gap-2 ">
-                        <StatsDetail
-                          icon={<ControllerIcon />}
-                          title={t("games_per_day")}
-                          type="normal"
-                          amount={statsGameById?.data.numnber_game_play || 0}
-                          unit={t("Games")}
-                        />
-                        <StatsDetail
-                          icon={<BankIcon />}
-                          title={t("costs_per_game")}
-                          type="range"
-                          amount={statsGameById?.data.cost_per_game_doller || 0}
-                          unit={`= ${
-                            statsGameById?.data.cost_per_game_naka || 0
-                          }`}
-                        />
-                      </div>
-                      <div className="col-span-2">
-                        <StatEstimatedProfit
-                          minValue={`+${
-                            statsGameById?.data.profit_potential_min || 0
-                          }%`}
-                          maxValue={`+${
-                            statsGameById?.data.profit_potential_max || 0
-                          }%`}
-                        />
-                      </div>
-                    </div>
-                    <div className="col-span-2">
-                      <TopPlayer
-                        element="select"
-                        subtitle
-                        background="neutral"
-                        note
-                        elevation={0}
-                        className="h-auto w-full border border-neutral-900 border-opacity-80 !bg-warning-contrastText "
-                        rank
-                        topPlayerGameId={weeklyPoolByGameId?.record || []}
-                        rightContent={
-                          <div className="flex h-10 items-center rounded-[20px] border-[1px] border-neutral-700">
-                            <button
-                              type="button"
-                              className={buttonArrow}
-                              onClick={() =>
-                                onClickedPrev(
-                                  weeklyPoolByGameId?.previous || ""
-                                )
-                              }
-                            >
-                              <IconArrowLeft />
-                            </button>
-                            <button
-                              type="button"
-                              className={`${buttonArrow} border-l-[1px] border-neutral-700`}
-                              onClick={() =>
-                                onClickedNext(weeklyPoolByGameId?.next || "")
-                              }
-                            >
-                              <IconArrowRight />
-                            </button>
-                          </div>
+            <Box component="section">
+              <div className="flex flex-wrap gap-3 xl:flex-row xl:flex-nowrap">
+                <LikeNoLobby
+                  imgSrc={
+                    gameData && "image_category_list" in gameData
+                      ? gameData.image_category_list
+                      : ""
+                  }
+                  value={78.34}
+                />
+                <StatisticGameDetail statsGameById={statsGameById} />
+                <TopPlayer
+                  element="select"
+                  subtitle
+                  background="neutral"
+                  note
+                  elevation={0}
+                  className="border border-neutral-800 bg-primary-main lg:!h-[424px]"
+                  rank
+                  topPlayerGameId={weeklyPoolByGameId?.record || []}
+                  isFetching={isLoadingWeeklyPoolByGameId}
+                  rightContent={
+                    <div className="flex h-10 items-center rounded-[20px] border-[1px] border-neutral-700">
+                      <button
+                        type="button"
+                        className={buttonArrow}
+                        onClick={() =>
+                          onClickedPrev(weeklyPoolByGameId?.previous || "")
                         }
-                      />
+                      >
+                        <IconArrowLeft />
+                      </button>
+                      <button
+                        type="button"
+                        className={`${buttonArrow} border-l-[1px] border-neutral-700`}
+                        onClick={() =>
+                          onClickedNext(weeklyPoolByGameId?.next || "")
+                        }
+                      >
+                        <IconArrowRight />
+                      </button>
                     </div>
-                  </div>
-                </Box>
-              </>
-            ) : (
-              <Box component="section">
-                <div className="flex flex-wrap gap-3 xl:flex-row xl:flex-nowrap">
-                  <LikeNoLobby
-                    imgSrc={
-                      gameData && "image_category_list" in gameData
-                        ? gameData.image_category_list
-                        : ""
-                    }
-                    value={78.34}
-                  />
-                  <StatisticGameDetail statsGameById={statsGameById} />
-                  <TopPlayer
-                    element="select"
-                    subtitle
-                    background="neutral"
-                    note
-                    elevation={0}
-                    className="border border-neutral-800 bg-primary-main lg:!h-[424px]"
-                    rank
-                    topPlayerGameId={weeklyPoolByGameId?.record || []}
-                    isFetching={isLoadingWeeklyPoolByGameId}
-                    rightContent={
-                      <div className="flex h-10 items-center rounded-[20px] border-[1px] border-neutral-700">
-                        <button
-                          type="button"
-                          className={buttonArrow}
-                          onClick={() =>
-                            onClickedPrev(weeklyPoolByGameId?.previous || "")
-                          }
-                        >
-                          <IconArrowLeft />
-                        </button>
-                        <button
-                          type="button"
-                          className={`${buttonArrow} border-l-[1px] border-neutral-700`}
-                          onClick={() =>
-                            onClickedNext(weeklyPoolByGameId?.next || "")
-                          }
-                        >
-                          <IconArrowRight />
-                        </button>
-                      </div>
-                    }
-                  />
-                </div>
-              </Box>
-            )}
+                  }
+                />
+              </div>
+            </Box>
           </div>
         )
     }

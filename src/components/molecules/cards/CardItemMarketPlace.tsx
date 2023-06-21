054a8fff9501a4c7757d135983163748ee8af5b9
rrrd1998 <buey@kos.co.th>
2023-06-21 16:51:30 +0700

    fix: remove useState

--- src/components/molecules/cards/CardItemMarketPlace.tsx
+++ src/components/molecules/cards/CardItemMarketPlace.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React from "react"
 import { motion } from "framer-motion"
 import ILogoMaster from "@components/icons/LogoMaster"
 import { Chip, Typography } from "@mui/material"

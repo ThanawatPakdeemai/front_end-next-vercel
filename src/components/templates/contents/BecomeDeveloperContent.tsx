3a492015973616ea988b1c07005a34450987a852
F.Boy <>
2023-07-21 14:16:17 +0700

    fix:update crud becomedev

--- src/components/templates/contents/BecomeDeveloperContent.tsx
+++ src/components/templates/contents/BecomeDeveloperContent.tsx
@@ -20,7 +20,6 @@ const BecomeDeveloperContent = ({
   children = (
     <Jumbotron
       detail="NAKAMOTO.GAMES"
-      textTitleSub="FOR GAME DEVELOPERS_"
       text="We take care of the infrastructure and distribution so you can focus on creating games. Publish your Web3 game now ⚡"
       textButton="Subscribe Now"
       className="w-[620px]"

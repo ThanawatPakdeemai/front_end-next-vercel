acf91ea6c60af1755e197045d314bf1ac8504275
F.Boy <>
2023-08-30 17:16:11 +0700

    fix:update-2

--- src/features/home/containers/hook/useGetGames/index.ts
+++ src/features/home/containers/hook/useGetGames/index.ts
@@ -1,27 +1,39 @@
 import { useQuery } from "@tanstack/react-query"
 import { getHomeSlide } from "@src/features/home/containers/services/home.service"
+import { useOfflineNotification } from "@hooks/worker"
 
 const useGetGames = () => {
+  // Destructure the result of useOfflineNotification directly
+  const [isOnline] = useOfflineNotification()
+
   const {
     data: slideGames,
     error,
     isLoading,
     isError
   } = useQuery(["slideGames"], getHomeSlide, {
-    staleTime: Infinity,
-    cacheTime: 5 * 60 * 1000,
-    onError: (_error) => {
-      // eslint-disable-next-line no-console
-      console.log("test-onError", _error)
-    }
+    staleTime: Infinity
   })
 
+  // Function to determine the appropriate data based on online/offline status
+  const determineData = () => {
+    const cachedData = JSON.parse(localStorage.getItem("homeSlide") || "{}")
+
+    if (!isOnline && cachedData && cachedData.length > 0) {
+      return cachedData
+    }
+    if (slideGames && slideGames.length > 0) {
+      return slideGames
+    }
+    return undefined
+  }
+
+  const slideGamesData = determineData()
+
   return {
-    slideGames: slideGames && slideGames.length > 0 ? slideGames : undefined,
+    slideGames: slideGamesData,
     error,
     isLoading,
     isError
   }
 }
-
-export default useGetGames
